eeebf63326d20b75dab5395ed8df5f6511f1c1a9
Alessandro Giusa <alessandrogiusa@gmail.com>
2016-07-07 13:11:23 +0200

    Added package-info and java doc
    * Added Java doc for FunctionalUtils
    * Added package-info for package org.iobserve.analysis.utils

--- org.iobserve.analysis/src/main/java/org/iobserve/analysis/utils/FunctionalUtils.java
+++ org.iobserve.analysis/src/main/java/org/iobserve/analysis/utils/FunctionalUtils.java
@@ -3,8 +3,17 @@ package org.iobserve.analysis.utils;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public final class FunctionalUtils {
+/**
+ * Utility class for functional style programming helper methods.
+ * @author Robert Heinrich
+ * @author Alessandro Giusa
+ *
+ */
+public final class FunctionalUtils { 
 
+	/**
+	 * Private constructor, since this class is an utility class.
+	 */
 	private FunctionalUtils() {
 		// no implementation
 	}
@@ -12,9 +21,13 @@ public final class FunctionalUtils {
 	/**
 	 * Convert the given two parameter function into a curry equivalent one.
 	 * @param function function to convert
+	 * @param <X> input first function
+	 * @param <Y> input second function
+	 * @param <Z> output second function
 	 * @return curried function
 	 */
-	public static <X, Y, Z> Function<X, Function<Y, Z>> curry(final BiFunction<X, Y, Z> function) {
+	public static <X, Y, Z> Function<X, Function<Y, Z>> curry(
+			final BiFunction<X, Y, Z> function) {
 		return (final X x) -> (final Y y) -> function.apply(x, y);
 	}
 
