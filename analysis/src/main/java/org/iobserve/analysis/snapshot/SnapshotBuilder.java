29d788ea3f113c22d89c79367fcc5d0d877ca655
SirVietnam <teigerweimann@web.de>
2017-04-03 17:01:04 +0200

    adding SnapshotBuilder, not yet Connected into teetime framework

---
+++ analysis/src/main/java/org/iobserve/analysis/snapshot/SnapshotBuilder.java
@@ -0,0 +1,75 @@
+package org.iobserve.analysis.snapshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.IIOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.iobserve.analysis.InitializeModelProviders;
+import org.iobserve.analysis.data.AddAllocationContextEvent;
+import org.iobserve.analysis.data.RemoveAllocationContextEvent;
+import org.iobserve.analysis.model.AbstractModelProvider;
+import org.iobserve.analysis.model.ResourceEnvironmentModelProvider;
+
+import teetime.framework.AbstractStage;
+import teetime.framework.InputPort;
+import teetime.framework.OutputPort;
+
+public class SnapshotBuilder extends AbstractStage {
+
+	private static boolean createSnapshot;
+	private final URI snapshotURI;
+	private final InitializeModelProviders modelProviders;
+	
+    private final InputPort inputPort = super.createInputPort();
+    private final OutputPort outputPort = super.createOutputPort();
+
+    
+	public SnapshotBuilder(final URI snapshotURI, final InitializeModelProviders modelProviders) {
+		super();
+
+		this.snapshotURI = snapshotURI;
+		this.modelProviders = modelProviders;
+
+	}
+
+	@Override
+	protected void execute() throws Exception {
+		if (SnapshotBuilder.createSnapshot) {
+			this.createModelSnapshot(modelProviders.getAllocationModelProvider());
+			this.createModelSnapshot(modelProviders.getRepositoryModelProvider());
+			this.createModelSnapshot(modelProviders.getResourceEnvironmentModelProvider());
+			this.createModelSnapshot(modelProviders.getSystemModelProvider());
+			this.createModelSnapshot(modelProviders.getUsageModelProvider());
+			// this.createModelSnapshot(modelProviders.getCorrespondenceModel());
+			SnapshotBuilder.createSnapshot = false;
+			
+			outputPort.send(null);
+		}
+	}
+
+	private void createModelSnapshot(AbstractModelProvider modelProvider) throws IOException {
+		URI modelURI = modelProvider.getModelUri();
+
+		File modelFile = new File(modelURI.path());
+		if (!modelFile.exists()) {
+			throw new IIOException("The given file URI did not point to a file");
+		}
+		String fileName = modelFile.getName();
+
+		URI reModelUri = URI.createFileURI(this.snapshotURI.path() + File.separator + fileName);
+		modelProvider.save(reModelUri);
+	}
+
+	public static void setSnapshotFlag() {
+		SnapshotBuilder.createSnapshot = true;
+	}
+	
+	
+	public InputPort getInputPort()
+	{
+		return this.inputPort;
+	}
+
+}
