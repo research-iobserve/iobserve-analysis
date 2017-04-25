852468a04d5ea3cd06b05e848116128750896bab
Tobias Pöppke <t.poeppke@gmx.de>
2017-04-25 02:24:23 +0200

    Added headless PerOpteryx plugin

---
+++ peropteryx.plugin/src/org/iobserve/planning/peropteryx/plugin/PerOpteryxLaunchConfigurationBuilder.java
@@ -0,0 +1,356 @@
+package org.iobserve.planning.peropteryx.plugin;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.emf.common.util.EList;
+import org.palladiosimulator.analyzer.workflow.ConstantsContainer;
+import org.palladiosimulator.edp2.EDP2Plugin;
+import org.palladiosimulator.edp2.impl.RepositoryManager;
+import org.palladiosimulator.edp2.models.Repository.LocalMemoryRepository;
+import org.palladiosimulator.edp2.models.Repository.Repository;
+import org.palladiosimulator.edp2.models.Repository.RepositoryFactory;
+import org.palladiosimulator.recorderframework.edp2.config.EDP2RecorderConfigurationFactory;
+import org.palladiosimulator.recorderframework.utils.RecorderExtensionHelper;
+import org.palladiosimulator.reliability.MarkovEvaluationType;
+import org.palladiosimulator.solver.runconfig.MessageStrings;
+
+import de.uka.ipd.sdq.dsexplore.launch.DSEConstantsContainer;
+import de.uka.ipd.sdq.dsexplore.launch.DSEConstantsContainer.QualityAttribute;
+import de.uka.ipd.sdq.simucomframework.SimuComConfig;
+import de.uka.ipd.sdq.simulation.AbstractSimulationConfig;
+
+public class PerOpteryxLaunchConfigurationBuilder {
+	private static final Logger LOG = Logger.getLogger(PerOpteryxLaunchConfigurationBuilder.class);
+
+	public static final String DSE_LAUNCH_TYPE_ID = "de.uka.ipd.sdq.dsexplore.launchDSE";
+	public static final String DEFAULT_LAUNCH_CONFIG_NAME = "iobserve-peropteryx";
+	public static final String DEFAULT_PROJECT_WORKING_DIR = "working_dir";
+	public static final String DEFAULT_PROJECT_NAME = "iObserve-PerOpteryx";
+
+	private PerOpteryxLaunchConfigurationBuilder(final Map<String, Object> attr) {
+		// not to be instantiated
+	}
+
+	/**
+	 * Creates a new Path for the given model inside the models root directory
+	 * and returns it to the caller.
+	 *
+	 * @param modelDir
+	 *            - The directory relative to the workspace root where the model
+	 *            is stored. May end with a '/' or without.
+	 * @param modelName
+	 * @param fileExtension
+	 *            - The file extension of the specific model including a dot,
+	 *            e.g. .allocation for an allocation model file.
+	 * @return
+	 */
+	private static IPath getModelFilePath(String modelDir, String modelName, String fileExtension) {
+		String modelFile;
+
+		if (modelDir.endsWith("/")) {
+			modelFile = "platform:/resource/" + modelDir + modelName + fileExtension;
+		} else {
+			modelFile = "platform:/resource/" + modelDir + "/" + modelName + fileExtension;
+		}
+
+		IPath filePath = new Path(modelFile);
+		return filePath;
+	}
+
+	public static ILaunchConfiguration getDefaultLaunchConfiguration(String projectModelDir, String modelName,
+			String sourceModelDir) throws CoreException {
+		final Map<String, Object> attr = new HashMap<>();
+
+		setDefaultConfigFiles(projectModelDir, modelName, attr);
+
+		setDefaultGeneralOptions(attr);
+
+		setDefaultAnalysisOptions(attr);
+
+		setDefaultResourceOptions(attr);
+
+		setDefaultReliabilityOptions(attr);
+
+		// setSimuComDefaultOptions(attr);
+		setLQNSDefaultOptions(attr, projectModelDir, sourceModelDir);
+
+		setDefaultTacticsOptions(attr);
+
+		ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
+		ILaunchConfigurationType launchConfigType = launchManager.getLaunchConfigurationType(DSE_LAUNCH_TYPE_ID);
+		ILaunchConfigurationWorkingCopy workingCopy = launchConfigType.newInstance(null, DEFAULT_LAUNCH_CONFIG_NAME);
+
+		workingCopy.setAttributes(attr);
+
+		ILaunchConfiguration launchConfig = workingCopy.doSave();
+
+		LOG.info("Using launch configuration with attributes: " + attr.toString());
+
+		return launchConfig;
+	}
+
+	private static void setDefaultTacticsOptions(Map<String, Object> attr) {
+		attr.put(DSEConstantsContainer.USE_REALLOCATION, false);
+		attr.put(DSEConstantsContainer.REALLOCATION_UTILISATION_DIFFERENCE, Double.toString(0.5));
+		attr.put(DSEConstantsContainer.REALLOCATION_WEIGHT, Double.toString(1.0));
+
+		attr.put(DSEConstantsContainer.USE_PROCESSING_RATE, false);
+		attr.put(DSEConstantsContainer.PROCESSING_RATE_DECREASE_FACTOR, Double.toString(0.25));
+		attr.put(DSEConstantsContainer.PROCESSING_RATE_INCREASE_FACTOR, Double.toString(0.25));
+		attr.put(DSEConstantsContainer.PROCESSING_RATE_THRESHOLD_HIGH_UTILISATION, Double.toString(0.8));
+		attr.put(DSEConstantsContainer.PROCESSING_RATE_THRESHOLD_LOW_UTILISATION, Double.toString(0.2));
+		attr.put(DSEConstantsContainer.PROCESSING_RATE_WEIGHT, Double.toString(0.1));
+
+		attr.put(DSEConstantsContainer.USE_SERVER_CONSOLIDATION, false);
+		attr.put(DSEConstantsContainer.SERVER_CONSOLIDATION_THRESHOLD_LOW_UTILISATION, Double.toString(0.3));
+		attr.put(DSEConstantsContainer.SERVER_CONSOLIDATION_WEIGHT, Double.toString(0.5));
+
+		attr.put(DSEConstantsContainer.USE_SERVER_EXPANSION, false);
+		attr.put(DSEConstantsContainer.SERVER_EXPANSION_MAX_NUMBER_OF_REPLACEMENTS, Integer.toString(1));
+		attr.put(DSEConstantsContainer.SERVER_EXPANSION_THRESHOLD_HIGH_UTILISATION, Double.toString(0.7));
+		attr.put(DSEConstantsContainer.SERVER_EXPANSION_WEIGHT, Double.toString(0.7));
+
+		attr.put(DSEConstantsContainer.USE_ANTIPATTERNS, false);
+	}
+
+	private static void setSimuComDefaultOptions(final Map<String, Object> attr) {
+		attr.put(SimuComConfig.SIMULATE_LINKING_RESOURCES, false);
+		attr.put(SimuComConfig.SIMULATE_THROUGHPUT_OF_LINKING_RESOURCES, true);
+		attr.put(SimuComConfig.SIMULATE_FAILURES, false);
+		attr.put(ConstantsContainer.FEATURE_CONFIG, ConstantsContainer.DEFAULT_FEATURE_CONFIGURATION_FILE);
+
+		attr.put(AbstractSimulationConfig.SIMULATOR_ID, AbstractSimulationConfig.DEFAULT_SIMULATOR_ID);
+		attr.put(AbstractSimulationConfig.EXPERIMENT_RUN, AbstractSimulationConfig.DEFAULT_EXPERIMENT_RUN);
+		attr.put(AbstractSimulationConfig.VARIATION_ID, AbstractSimulationConfig.DEFAULT_VARIATION_NAME);
+		attr.put(AbstractSimulationConfig.SIMULATION_TIME, AbstractSimulationConfig.DEFAULT_SIMULATION_TIME);
+		attr.put(AbstractSimulationConfig.MAXIMUM_MEASUREMENT_COUNT,
+				AbstractSimulationConfig.DEFAULT_MAXIMUM_MEASUREMENT_COUNT);
+		attr.put(AbstractSimulationConfig.PERSISTENCE_RECORDER_NAME,
+				AbstractSimulationConfig.DEFAULT_PERSISTENCE_RECORDER_NAME);
+		attr.put(AbstractSimulationConfig.USE_FIXED_SEED, false);
+		attr.put(SimuComConfig.USE_CONFIDENCE, SimuComConfig.DEFAULT_USE_CONFIDENCE);
+		attr.put(SimuComConfig.CONFIDENCE_LEVEL, Integer.toString(SimuComConfig.DEFAULT_CONFIDENCE_LEVEL));
+		attr.put(SimuComConfig.CONFIDENCE_HALFWIDTH, Integer.toString(SimuComConfig.DEFAULT_CONFIDENCE_HALFWIDTH));
+		attr.put(SimuComConfig.CONFIDENCE_MODELELEMENT_NAME, SimuComConfig.DEFAULT_CONFIDENCE_MODELELEMENT_NAME);
+		attr.put(SimuComConfig.CONFIDENCE_MODELELEMENT_URI, SimuComConfig.DEFAULT_CONFIDENCE_MODELELEMENT_URI);
+		attr.put(SimuComConfig.CONFIDENCE_USE_AUTOMATIC_BATCHES,
+				SimuComConfig.DEFAULT_CONFIDENCE_USE_AUTOMATIC_BATCHES);
+		attr.put(SimuComConfig.CONFIDENCE_BATCH_SIZE, Integer.toString(SimuComConfig.DEFAULT_CONFIDENCE_BATCH_SIZE));
+		attr.put(SimuComConfig.CONFIDENCE_MIN_NUMBER_OF_BATCHES,
+				SimuComConfig.DEFAULT_CONFIDENCE_MIN_NUMBER_OF_BATCHES);
+		attr.put(SimuComConfig.VERBOSE_LOGGING, false);
+		attr.put(ConstantsContainer.DELETE_TEMPORARY_DATA_AFTER_ANALYSIS, true);
+
+		setEDP2ResourceRepository(attr);
+
+		setDefaultPersistenceRecorder(attr);
+	}
+
+	private static void setLQNSDefaultOptions(final Map<String, Object> attr, String projectModelDir,
+			String sourceModelDir) {
+		LOG.info("sourceModelDir: " + sourceModelDir);
+		(new File(sourceModelDir + "/lqns_out")).mkdirs();
+		attr.put(MessageStrings.SOLVER, MessageStrings.LQNS_SOLVER);
+		attr.put(MessageStrings.LQNS_OUTPUT, MessageStrings.LQN_OUTPUT_XML);
+
+		attr.put(MessageStrings.SAMPLING_DIST, Double.toString(1.0));
+		attr.put(MessageStrings.MAX_DOMAIN, 256);
+
+		attr.put(MessageStrings.CONV_VALUE, Double.toString(0.001));
+		attr.put(MessageStrings.IT_LIMIT, Integer.toString(50));
+		attr.put(MessageStrings.PRINT_INT, Integer.toString(10));
+		attr.put(MessageStrings.UNDER_COEFF, Double.toString(0.5));
+		attr.put(MessageStrings.LQNS_OUTPUT, sourceModelDir + "/lqns_out/output");
+		attr.put(MessageStrings.LQSIM_OUTPUT, sourceModelDir + "/lqsim_out/output");
+
+		attr.put(MessageStrings.STOP_ON_MESSAGE_LOSS_LQNS, true);
+		attr.put(MessageStrings.STOP_ON_MESSAGE_LOSS_LQSIM, true);
+		attr.put(MessageStrings.DEBUG_LINE, true);
+
+		attr.put(MessageStrings.INFINITE_TASK_MULTIPLICITY, true);
+
+		attr.put(MessageStrings.RUN_TIME, "");
+		attr.put(MessageStrings.BLOCKS, "");
+		attr.put(MessageStrings.PS_QUANTUM, Double.toString(0.001));
+
+		attr.put(MessageStrings.PRAGMAS, "");
+
+		attr.put(MessageStrings.LQNS_OUTPUT_DIR, sourceModelDir + "/lqns_out");
+		attr.put(MessageStrings.LQSIM_OUTPUT_DIR, sourceModelDir + "/lqsim_out");
+		attr.put(MessageStrings.SRE_OUTPUT_FILE, sourceModelDir + "/sre_out");
+		attr.put(MessageStrings.LINE_OUT_DIR, sourceModelDir + "/line_out");
+		attr.put(MessageStrings.LINE_PROP_FILE, sourceModelDir + "/" + MessageStrings.LINE_PROP_FILENAME);
+
+		attr.put(MessageStrings.SRE_IS_USE_INPUT_MODEL, false);
+
+		attr.put(ConstantsContainer.FEATURE_CONFIG, ConstantsContainer.DEFAULT_FEATURE_CONFIGURATION_FILE);
+
+		attr.put(ConstantsContainer.DELETE_TEMPORARY_DATA_AFTER_ANALYSIS, true);
+
+		// setEDP2ResourceRepository(attr);
+
+		// setDefaultPersistenceRecorder(attr);
+	}
+
+	private static void setDefaultPersistenceRecorder(final Map<String, Object> attr) {
+		// set default value for persistence framework
+		final List<String> recorderNames = RecorderExtensionHelper.getRecorderNames();
+
+		String edp2RecorderName = null;
+		for (String name : recorderNames) {
+			if (name.contains("EDP2")) {
+				edp2RecorderName = name;
+				break;
+			}
+		}
+
+		if (edp2RecorderName == null) {
+			if (recorderNames.size() > 0) {
+				attr.put(AbstractSimulationConfig.PERSISTENCE_RECORDER_NAME, recorderNames.get(0));
+			} else {
+				throw new IllegalArgumentException("No persistence recorder framework could be found!");
+			}
+		} else {
+			attr.put(AbstractSimulationConfig.PERSISTENCE_RECORDER_NAME, edp2RecorderName);
+		}
+	}
+
+	private static void setEDP2ResourceRepository(final Map<String, Object> attr) {
+		EList<Repository> repositories = EDP2Plugin.INSTANCE.getRepositories().getAvailableRepositories();
+		if (repositories.size() > 0) {
+			attr.put(EDP2RecorderConfigurationFactory.REPOSITORY_ID, repositories.get(0).getId());
+		} else {
+			LocalMemoryRepository repository = RepositoryFactory.eINSTANCE.createLocalMemoryRepository();
+			RepositoryManager.addRepository(EDP2Plugin.INSTANCE.getRepositories(), repository);
+			attr.put(EDP2RecorderConfigurationFactory.REPOSITORY_ID, repository.getId());
+		}
+	}
+
+	private static void setDefaultReliabilityOptions(final Map<String, Object> attr) {
+		attr.put(MessageStrings.MARKOV_STATISTICS, false);
+		attr.put(MessageStrings.SINGLE_RESULTS, false);
+		attr.put(MessageStrings.NUMBER_OF_EVALUATED_SYSTEM_STATES_ENABLED, false);
+		attr.put(MessageStrings.NUMBER_OF_EVALUATED_SYSTEM_STATES, 1);
+		attr.put(MessageStrings.NUMBER_OF_EXACT_DECIMAL_PLACES_ENABLED, false);
+		attr.put(MessageStrings.NUMBER_OF_EXACT_DECIMAL_PLACES, 1);
+		attr.put(MessageStrings.SOLVING_TIME_LIMIT_ENABLED, false);
+		attr.put(MessageStrings.SOLVING_TIME_LIMIT, 1);
+		attr.put(MessageStrings.LOG_FILE, "");
+		attr.put(MessageStrings.MARKOV_MODEL_REDUCTION_ENABLED, true);
+		attr.put(MessageStrings.MARKOV_MODEL_TRACES_ENABLED, false);
+		attr.put(MessageStrings.ITERATION_OVER_PHYSICAL_SYSTEM_STATES_ENABLED, true);
+		attr.put(MessageStrings.MARKOV_MODEL_STORAGE_ENABLED, false);
+		attr.put(MessageStrings.MARKOV_MODEL_FILE, "");
+		attr.put(MessageStrings.MARKOV_EVALUATION_MODE, MarkovEvaluationType.POINTSOFFAILURE.toString());
+	}
+
+	private static void setDefaultResourceOptions(final Map<String, Object> attr) {
+		attr.put(DSEConstantsContainer.MIN_NUMBER_RESOURCE_CONTAINERS, Integer.toString(2));
+		attr.put(DSEConstantsContainer.MAX_NUMBER_RESOURCE_CONTAINERS, Integer.toString(9));
+		attr.put(DSEConstantsContainer.NUMBER_OF_CANDIDATES_PER_ALLOCATION_LEVEL, Integer.toString(10));
+
+		attr.put(DSEConstantsContainer.REALLOCATION_UTILISATION_DIFFERENCE, Double.toString(0.5));
+		attr.put(DSEConstantsContainer.REALLOCATION_WEIGHT, Double.toString(1.0));
+	}
+
+	private static void setDefaultAnalysisOptions(final Map<String, Object> attr) {
+		attr.put(DSEConstantsContainer.getAnalysisMethod(QualityAttribute.PERFORMANCE_QUALITY), "LQN Solver Analysis");
+		attr.put(DSEConstantsContainer.getAnalysisMethod(QualityAttribute.COST_QUALITY), "Cost Analysis");
+		attr.put(DSEConstantsContainer.getAnalysisMethod(QualityAttribute.NQR_QUALITY), "none");
+		attr.put(DSEConstantsContainer.getAnalysisMethod(QualityAttribute.RELIABILITY_QUALITY),
+				"Reliability Solver Analysis");
+		attr.put(DSEConstantsContainer.getAnalysisMethod(QualityAttribute.SECURITY_QUALITY), "none");
+	}
+
+	private static void setDefaultGeneralOptions(final Map<String, Object> attr) {
+		attr.put(ConstantsContainer.ANALYSE_ACCURACY, ConstantsContainer.DEFAULT_ANALYSE_ACCURACY);
+		attr.put(ConstantsContainer.DO_SENSITIVITY_ANALYSIS, ConstantsContainer.DEFAULT_DO_SENSITIVITY_ANALYSIS);
+
+		attr.put(DSEConstantsContainer.SEARCH_METHOD, DSEConstantsContainer.SEARCH_EVOLUTIONARY);
+		attr.put(DSEConstantsContainer.OPTIMISATION_ONLY, true);
+		attr.put(DSEConstantsContainer.DESIGN_DECISIONS_ONLY, false);
+		attr.put(DSEConstantsContainer.STORE_RESULTS_AS_EMF, true);
+		attr.put(DSEConstantsContainer.STORE_RESULTS_AS_CSV, true);
+		attr.put(DSEConstantsContainer.TC_INSIGNIFICANT_FRONT_CHANGE_ACTIVATE, true);
+		attr.put(DSEConstantsContainer.DSE_ITERATIONS, "1");
+		attr.put(DSEConstantsContainer.CROSSOVER_RATE, "0.5");
+		attr.put(DSEConstantsContainer.PREDEFINED_INSTANCES, "");
+		attr.put(DSEConstantsContainer.CACHE_INSTANCES, "");
+		attr.put(DSEConstantsContainer.ALL_CANDIDATES, "");
+		attr.put(DSEConstantsContainer.ARCHIVE_CANDIDATES, "");
+		attr.put(DSEConstantsContainer.TACTICS_PROBABILITY, "0.6");
+		attr.put(DSEConstantsContainer.USE_REALLOCATION, false);
+		attr.put(DSEConstantsContainer.USE_SERVER_CONSOLIDATION, false);
+		attr.put(DSEConstantsContainer.USE_SERVER_EXPANSION, false);
+		attr.put(DSEConstantsContainer.USE_LINK_REALLOCATION, false);
+		attr.put(DSEConstantsContainer.USE_ANTIPATTERNS, false);
+		attr.put(DSEConstantsContainer.USE_STARTING_POPULATION_HEURISTIC, false);
+		attr.put(DSEConstantsContainer.STOP_ON_INITIAL_FAILURE, false);
+		attr.put(DSEConstantsContainer.INDIVIDUALS_PER_GENERATION, "3");
+	}
+
+	private static void setDefaultConfigFiles(String modelDir, String modelName, final Map<String, Object> attr) {
+		// Standard PCM settings
+		IPath allocationFile = getModelFilePath(modelDir, modelName, ".allocation");
+		LOG.info("Allocation file: " + allocationFile);
+		attr.put(ConstantsContainer.ALLOCATION_FILE, allocationFile.toString());
+
+		IPath outputFolder = new Path("output");
+		LOG.info("Output folder: " + outputFolder);
+		attr.put(ConstantsContainer.CLIENTOUT_PATH, outputFolder.toString());
+
+		IPath repositoryFile = getModelFilePath(modelDir, modelName, ".repository");
+		LOG.info("Repository file: " + repositoryFile);
+		attr.put(ConstantsContainer.REPOSITORY_FILE, repositoryFile.toString());
+
+		IPath resourceEnvFile = getModelFilePath(modelDir, modelName, ".resourceenvironment");
+		LOG.info("Resourceenvironment file: " + resourceEnvFile);
+		attr.put(ConstantsContainer.RESOURCEENVIRONMENT_FILE, resourceEnvFile.toString());
+
+		IPath systemFile = getModelFilePath(modelDir, modelName, ".system");
+		LOG.info("System file: " + systemFile);
+		attr.put(ConstantsContainer.SYSTEM_FILE, systemFile.toString());
+
+		// Has to be only the name because PerOpteryx will create a project with
+		// this name
+		IPath tempFolder = new Path("temporary");
+		LOG.info("Temp folder: " + tempFolder);
+		attr.put(ConstantsContainer.TEMPORARY_DATA_LOCATION, tempFolder.toString());
+
+		IPath usageFile = getModelFilePath(modelDir, modelName, ".usagemodel");
+		LOG.info("Usage file: " + usageFile);
+		attr.put(ConstantsContainer.USAGE_FILE, usageFile.toString());
+
+		attr.put(ConstantsContainer.ACCURACY_QUALITY_ANNOTATION_FILE,
+				ConstantsContainer.DEFAULT_ACCURACY_QUALITY_ANNOTATION_FILE);
+		attr.put(ConstantsContainer.RMI_MIDDLEWARE_REPOSITORY_FILE,
+				ConstantsContainer.DEFAULT_RMI_MIDDLEWARE_REPOSITORY_FILE);
+		attr.put(ConstantsContainer.EVENT_MIDDLEWARE_REPOSITORY_FILE, ConstantsContainer.DEFAULT_EVENT_MIDDLEWARE_FILE);
+		attr.put(ConstantsContainer.FEATURE_CONFIG, ConstantsContainer.DEFAULT_FEATURE_CONFIGURATION_FILE);
+
+		// PerOpteryx specific settings
+		IPath costFile = getModelFilePath(modelDir, modelName, ".cost");
+		LOG.info("Cost file: " + costFile);
+		attr.put(DSEConstantsContainer.COST_FILE, costFile.toString());
+
+		IPath decisionFile = getModelFilePath(modelDir, modelName, ".designdecision");
+		LOG.info("Designdecision file: " + decisionFile);
+		attr.put(DSEConstantsContainer.DESIGN_DECISION_FILE, decisionFile.toString());
+
+		IPath qmlFile = getModelFilePath(modelDir, modelName, ".qmldeclarations");
+		LOG.info("QMLDeclarations file: " + qmlFile);
+		attr.put(DSEConstantsContainer.QML_DEFINITION_FILE, qmlFile.toString());
+	}
+}
