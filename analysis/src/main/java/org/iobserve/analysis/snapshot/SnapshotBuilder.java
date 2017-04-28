8477440130398d4c77179c22f2d63ba9d618fc71
Philipp Weimann <teigerweimann@web.de>
2017-04-28 15:57:51 +0200

    changed method of providing the snapshot from model "SaveAs"-Principle to "CopyFile"-Principle

--- analysis/src/main/java/org/iobserve/analysis/snapshot/SnapshotBuilder.java
+++ analysis/src/main/java/org/iobserve/analysis/snapshot/SnapshotBuilder.java
@@ -2,6 +2,9 @@ package org.iobserve.analysis.snapshot;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import javax.imageio.IIOException;
 
 import org.eclipse.emf.common.util.URI;
@@ -14,29 +17,29 @@ import teetime.framework.OutputPort;
 
 public class SnapshotBuilder extends AbstractStage {
 
+	private static CopyOption[] copyOptions = new CopyOption[] { StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES };
+
 	private static boolean createSnapshot;
 	private final URI snapshotURI;
 	private final InitializeModelProviders modelProviders;
-	
-//    private final InputPort<Boolean> inputPort = super.createInputPort();
-    private final OutputPort<URI> outputPort = super.createOutputPort();
 
-    
+	// private final InputPort<Boolean> inputPort = super.createInputPort();
+	private final OutputPort<URI> outputPort = super.createOutputPort();
+
 	public SnapshotBuilder(final URI snapshotURI, final InitializeModelProviders modelProviders) {
 		super();
 
 		SnapshotBuilder.createSnapshot = false;
 		this.snapshotURI = snapshotURI;
 		this.modelProviders = modelProviders;
-
 	}
 
 	@Override
 	protected void execute() throws Exception {
-		//TODO fix this and make .recieve() possible!
-//		List<InputPort<?>> inputPorts = super.getInputPorts();
-//		Boolean createSnapshot = this.inputPort.receive();
-		
+		// TODO fix this and make .recieve() possible!
+		// List<InputPort<?>> inputPorts = super.getInputPorts();
+		// Boolean createSnapshot = this.inputPort.receive();
+
 		if (SnapshotBuilder.createSnapshot) {
 			this.createModelSnapshot(modelProviders.getAllocationModelProvider());
 			this.createModelSnapshot(modelProviders.getRepositoryModelProvider());
@@ -45,13 +48,14 @@ public class SnapshotBuilder extends AbstractStage {
 			this.createModelSnapshot(modelProviders.getUsageModelProvider());
 			// this.createModelSnapshot(modelProviders.getCorrespondenceModel());
 			SnapshotBuilder.createSnapshot = false;
-			
+
 			outputPort.send(this.snapshotURI);
 		}
 	}
 
-
 	private void createModelSnapshot(AbstractModelProvider<?> modelProvider) throws IOException {
+		modelProvider.save();
+
 		URI modelURI = modelProvider.getModelUri();
 
 		File modelFile = new File(modelURI.path());
@@ -59,18 +63,17 @@ public class SnapshotBuilder extends AbstractStage {
 			throw new IIOException("The given file URI did not point to a file");
 		}
 		String fileName = modelFile.getName();
+		String targetFileLocation = this.snapshotURI.toFileString() + File.separator + fileName;
+		File modelFileCopy = new File(targetFileLocation);
 
-		URI reModelUri = URI.createFileURI(this.snapshotURI.toFileString() + File.separator + fileName);
-		modelProvider.save(reModelUri);
+		Files.copy(modelFile.toPath(), modelFileCopy.toPath(), SnapshotBuilder.copyOptions);
 	}
 
 	public static void setSnapshotFlag() {
 		SnapshotBuilder.createSnapshot = true;
 	}
-	
-	
-	public InputPort<Boolean> getInputPort()
-	{
+
+	public InputPort<Boolean> getInputPort() {
 		return super.createInputPort();
 	}
 	
