78c2c1f3ca279b8e45907752255c3b726f6a167f
Lars Blümke <lbl@informatik.uni-kiel.de>
2017-05-16 15:22:34 +0200

    moved first design prototypes to package misc

--- analysis/src/main/java/org/iobserve/analysis/modelneo4j/misc/Neo4jRepositoryModelProvider.java
+++ analysis/src/main/java/org/iobserve/analysis/modelneo4j/misc/Neo4jRepositoryModelProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ***************************************************************************/
-package org.iobserve.analysis.modelneo4j.repositorymodel;
+package org.iobserve.analysis.modelneo4j.misc;
 
 import org.eclipse.emf.common.util.EList;
 import org.neo4j.graphdb.GraphDatabaseService;
