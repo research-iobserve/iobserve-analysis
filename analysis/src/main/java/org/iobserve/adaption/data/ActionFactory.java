e8201899412d6dbd4a5b95e11ef828f28a59c40d
Philipp Weimann <teigerweimann@web.de>
2017-05-20 11:26:44 +0200

    wip ActionFactories

--- analysis/src/main/java/org/iobserve/adaption/data/ActionFactory.java
+++ analysis/src/main/java/org/iobserve/adaption/data/ActionFactory.java
@@ -2,7 +2,9 @@ package org.iobserve.adaption.data;
 
 import org.iobserve.analysis.InitializeModelProviders;
 import org.iobserve.analysis.graph.ComponentNode;
+import org.iobserve.planning.systemadaptation.AllocateAction;
 import org.iobserve.planning.systemadaptation.ChangeRepositoryComponentAction;
+import org.iobserve.planning.systemadaptation.DeallocateAction;
 import org.iobserve.planning.systemadaptation.MigrateAction;
 import org.iobserve.planning.systemadaptation.systemadaptationFactory;
 import org.iobserve.planning.systemadaptation.impl.ChangeRepositoryComponentActionImpl;
@@ -12,60 +14,34 @@ import org.palladiosimulator.pcm.allocation.AllocationContext;
 import org.palladiosimulator.pcm.core.composition.AssemblyContext;
 import org.palladiosimulator.pcm.repository.Repository;
 import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
 import org.palladiosimulator.pcm.system.System;
 
 public class ActionFactory {
 
-	public static InitializeModelProviders models;
-
-	/**
-	 * 
-	 * @param runtimeNode
-	 * @param reDeploymentNode
-	 * @return
-	 */
-	public static ChangeRepositoryComponentAction generateChangeRepositoryComponentAction(ComponentNode runtimeNode, ComponentNode reDeploymentNode) {
-		systemadaptationFactory factory = systemadaptationFactoryImpl.eINSTANCE;
-		ChangeRepositoryComponentAction action = factory.createChangeRepositoryComponentAction();
-		
-		action.setSourceAssemblyContext(ActionFactory.getAssemblyContext(runtimeNode.getAssemblyContextID()));
-
-		Repository repositoryModel = models.getRepositoryModelProvider().getModel();
-		RepositoryComponent repositoryComponent = repositoryModel.getComponents__Repository().stream()
-				.filter(s -> s.getId() == reDeploymentNode.getRepositoryComponentID()).findFirst().get();
-		action.setNewRepositoryComponent(repositoryComponent);
-
-		return action;
-	}
-	
-	public static MigrateAction generateMigrateAction(ComponentNode runtimeNode, ComponentNode reDeploymentNode)
-	{
-		systemadaptationFactory factory = systemadaptationFactoryImpl.eINSTANCE;
-		MigrateAction action = factory.createMigrateAction();
-		
-		action.setSourceAssemblyContext(ActionFactory.getAssemblyContext(runtimeNode.getAssemblyContextID()));
-		
-		action.setNewAllocatinContext(ActionFactory.getAllocationContext(runtimeNode.getAssemblyContextID()));
-		return action;
-	}
-	
-	
+	protected static InitializeModelProviders runtimeModels;
+	protected static InitializeModelProviders redeploymentModels;
 
 	/*
 	 * ////////// HELPERS //////////
 	 */
-	private static AssemblyContext getAssemblyContext(String contextID) {
-		System systemModel = models.getSystemModelProvider().getModel();
-		AssemblyContext assemblyContext = systemModel.getAssemblyContexts__ComposedStructure().stream().filter(s -> s.getId() == contextID)
+	protected static AssemblyContext getAssemblyContext(String contextID, System systemModel) {
+		AssemblyContext assemblyContext = systemModel.getAssemblyContexts__ComposedStructure().stream().filter(s -> s.getId().equals(contextID))
 				.findFirst().get();
 		return assemblyContext;
 	}
-	
-	private static AllocationContext getAllocationContext(String allocationID)
-	{
-		Allocation allocationModel = models.getAllocationModelProvider().getModel();
-		AllocationContext allocationContext = allocationModel.getAllocationContexts_Allocation().stream().filter(s -> s.getId() == allocationID)
+
+	protected static AllocationContext getAllocationContext(String allocationID, Allocation allocationModel) {
+		AllocationContext allocationContext = allocationModel.getAllocationContexts_Allocation().stream().filter(s -> s.getId().equals(allocationID))
 				.findFirst().get();
 		return allocationContext;
+	}
+
+	protected static ResourceContainer getResourceContainer(String resourceContainerID, ResourceEnvironment resEnvModel) {
+		ResourceContainer resContainer = resEnvModel.getResourceContainer_ResourceEnvironment().stream()
+				.filter(s -> s.getId().equals(resourceContainerID)).findFirst().get();
+
+		return resContainer;
 	}
 }
