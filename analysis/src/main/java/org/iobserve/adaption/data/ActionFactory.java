d35ecfe98d89de5c8ac219b8219193974aa54da4
Philipp Weimann <teigerweimann@web.de>
2017-05-20 17:39:59 +0200

    fixed bugs at ActionFactory & AssemblyContextActionFactory
    (cherry picked from commit f8fb7f9a5911801e0b62ce202c3fe8327fede3b1)

--- analysis/src/main/java/org/iobserve/adaption/data/ActionFactory.java
+++ analysis/src/main/java/org/iobserve/adaption/data/ActionFactory.java
@@ -1,19 +1,9 @@
 package org.iobserve.adaption.data;
 
 import org.iobserve.analysis.InitializeModelProviders;
-import org.iobserve.analysis.graph.ComponentNode;
-import org.iobserve.planning.systemadaptation.AllocateAction;
-import org.iobserve.planning.systemadaptation.ChangeRepositoryComponentAction;
-import org.iobserve.planning.systemadaptation.DeallocateAction;
-import org.iobserve.planning.systemadaptation.MigrateAction;
-import org.iobserve.planning.systemadaptation.systemadaptationFactory;
-import org.iobserve.planning.systemadaptation.impl.ChangeRepositoryComponentActionImpl;
-import org.iobserve.planning.systemadaptation.impl.systemadaptationFactoryImpl;
 import org.palladiosimulator.pcm.allocation.Allocation;
 import org.palladiosimulator.pcm.allocation.AllocationContext;
 import org.palladiosimulator.pcm.core.composition.AssemblyContext;
-import org.palladiosimulator.pcm.repository.Repository;
-import org.palladiosimulator.pcm.repository.RepositoryComponent;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
 import org.palladiosimulator.pcm.system.System;
