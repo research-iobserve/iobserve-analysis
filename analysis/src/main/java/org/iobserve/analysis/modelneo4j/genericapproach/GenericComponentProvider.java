d392509c5c72d7a37a3cd39a113a4384040e280d
Lars Blümke <lbl@informatik.uni-kiel.de>
2017-05-24 11:31:02 +0200

    fixed writing for now

--- analysis/src/main/java/org/iobserve/analysis/modelneo4j/genericapproach/GenericComponentProvider.java
+++ analysis/src/main/java/org/iobserve/analysis/modelneo4j/genericapproach/GenericComponentProvider.java
@@ -16,7 +16,6 @@
 package org.iobserve.analysis.modelneo4j.genericapproach;
 
 import org.eclipse.emf.ecore.EAttribute;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.iobserve.analysis.modelneo4j.PcmRelationshipType;
@@ -24,6 +23,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+import org.palladiosimulator.pcm.core.entity.Entity;
 
 /**
  *
@@ -31,12 +31,13 @@ import org.neo4j.graphdb.Transaction;
  *
  * @param <T>
  */
-public class GenericComponentProvider<T extends EObject> {
+public class GenericComponentProvider<T extends Entity> {
 
     public static final String ID = "id";
     public static final String ENTITY_NAME = "entityName";
 
     private final GraphDatabaseService graph;
+    private Node node;
 
     public GenericComponentProvider(final GraphDatabaseService graph) {
         this.graph = graph;
@@ -45,69 +46,87 @@ public class GenericComponentProvider<T extends EObject> {
     public Node createComponent(final T component) {
         System.out.println("\n\n------------------------------------------------------------------------");
         System.out.println("component " + component);
+        final Label label = Label.label(this.parseTypeName(component.eClass().getInstanceTypeName()));
         try (Transaction tx = this.getGraph().beginTx()) {
-            final Label label = Label.label(component.eClass().getInstanceTypeName());
-            Node thisNode = this.getGraph().findNode(label, GenericComponentProvider.ID,
+            this.node = this.getGraph().findNode(label, GenericComponentProvider.ID,
                     component.eGet(component.eClass().getEIDAttribute()));
+            tx.success();
+        }
 
-            if (thisNode == null) {
-                thisNode = this.getGraph().createNode(label);
-
-                int as = 0;
-                int rs = 0;
-                for (final EStructuralFeature f : component.eClass().getEAllStructuralFeatures()) {
-                    if (f instanceof EAttribute) {
-                        as++;
-                        System.out.println("attibute " + ((EAttribute) f).getEAttributeType() + " key: " + f.getName()
-                                + " value: " + component.eGet(f));
-
-                        final Object a = component.eGet(f);
-                        if (a != null) {
-                            thisNode.setProperty(f.getName(), a.toString());
-                        }
-                    } else if (f instanceof EReference) {
-
-                        rs++;
-                        System.out.println(
-                                "\n\n------------------------------------------------------------------------");
-                        System.out.println("component " + component);
-                        System.out.println(
-                                "reference " + ((EReference) f).getEReferenceType() + " value: " + component.eGet(f));
-
-                        final Object r = component.eGet(f);
-                        if (r instanceof Iterable<?>) {
-                            System.out.println("iterable");
-                            for (final Object i : (Iterable<?>) component.eGet(f)) {
-                                if (i instanceof EObject) {
-
-                                    final Node refNode = new GenericComponentProvider<>(this.graph)
-                                            .createComponent((EObject) i);
-                                    thisNode.createRelationshipTo(refNode, PcmRelationshipType.REFERENCES);
-                                } else {
-                                    System.out.println(i + " should not happen!!!!!!!!!");
-                                }
-                            }
-
-                        } else {
-                            System.out.println("not iterable");
-                            if (r instanceof EObject) {
-
-                                final Node refNode = new GenericComponentProvider<>(this.graph)
-                                        .createComponent((EObject) r);
-                                thisNode.createRelationshipTo(refNode, PcmRelationshipType.REFERENCES);
-                            } else {
-                                System.out.println(r + " should not happen!!!!!!!!!");
-                            }
-                        }
-                    }
-                }
-                System.out.println(label + " has " + as + " attributes and " + rs + " references");
-
+        if (this.node == null) {
+            try (Transaction tx = this.getGraph().beginTx()) {
+                this.node = this.getGraph().createNode(label);
                 tx.success();
             }
 
-            return thisNode;
+            int as = 0;
+            int rs = 0;
+            for (final EStructuralFeature f : component.eClass().getEAllStructuralFeatures()) {
+                if (f instanceof EAttribute) {
+                    as++;
+                    System.out.println("attibute " + ((EAttribute) f).getEAttributeType() + " key: " + f.getName()
+                            + " value: " + component.eGet(f));
+
+                    final Object a = component.eGet(f);
+                    if (a != null) {
+                        try (Transaction tx = this.getGraph().beginTx()) {
+                            this.node.setProperty(f.getName(), a.toString());
+                            tx.success();
+                        }
+                    }
+
+                } else if (f instanceof EReference) {
+
+                    rs++;
+                    System.out.println("\n\n------------------------------------------------------------------------");
+                    System.out.println("component " + component);
+                    System.out.println(
+                            "reference " + ((EReference) f).getEReferenceType() + " value: " + component.eGet(f));
+
+                    final Object r = component.eGet(f);
+                    if (r instanceof Iterable<?>) {
+                        System.out.println("iterable");
+                        for (final Object i : (Iterable<?>) component.eGet(f)) {
+                            if (i instanceof Entity) {
+
+                                final Node refNode = new GenericComponentProvider<>(this.graph)
+                                        .createComponent((Entity) i);
+                                try (Transaction tx = this.getGraph().beginTx()) {
+                                    this.node.createRelationshipTo(refNode, PcmRelationshipType.REFERENCES);
+                                    tx.success();
+                                }
+                            } else {
+                                System.out.println(i + " should not happen!!!!!!!!!");
+                            }
+                        }
+
+                    } else {
+                        System.out.println("not iterable");
+                        if (r instanceof Entity) {
+
+                            final Node refNode = new GenericComponentProvider<>(this.graph).createComponent((Entity) r);
+                            try (Transaction tx = this.getGraph().beginTx()) {
+                                this.node.createRelationshipTo(refNode, PcmRelationshipType.REFERENCES);
+                                tx.success();
+                            }
+                        } else {
+                            System.out.println(r + " should not happen!!!!!!!!!");
+                        }
+                    }
+                }
+            }
+            System.out.println(label + " has " + as + " attributes and " + rs + " references");
+
         }
+
+        return this.node;
+
+    }
+
+    private String parseTypeName(final String name) {
+        final int i = name.lastIndexOf(".");
+        return name.substring(i + 1);
+
     }
 
     public T readComponent(final String id) {
