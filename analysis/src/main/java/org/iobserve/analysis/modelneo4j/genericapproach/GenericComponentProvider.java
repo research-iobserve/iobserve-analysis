ce3c04454ad8a4e6d6b50e7f486241533ed8048e
Lars Blümke <lbl@informatik.uni-kiel.de>
2017-06-01 16:27:40 +0200

    implemented a way to instantiate objects from different packages

--- analysis/src/main/java/org/iobserve/analysis/modelneo4j/genericapproach/GenericComponentProvider.java
+++ analysis/src/main/java/org/iobserve/analysis/modelneo4j/genericapproach/GenericComponentProvider.java
@@ -33,14 +33,50 @@ import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
+import org.palladiosimulator.pcm.allocation.AllocationFactory;
+import org.palladiosimulator.pcm.allocation.AllocationPackage;
+import org.palladiosimulator.pcm.core.CoreFactory;
+import org.palladiosimulator.pcm.core.CorePackage;
+import org.palladiosimulator.pcm.core.composition.CompositionFactory;
+import org.palladiosimulator.pcm.core.composition.CompositionPackage;
 import org.palladiosimulator.pcm.core.entity.Entity;
+import org.palladiosimulator.pcm.core.entity.EntityFactory;
+import org.palladiosimulator.pcm.core.entity.EntityPackage;
+import org.palladiosimulator.pcm.parameter.ParameterFactory;
+import org.palladiosimulator.pcm.parameter.ParameterPackage;
 import org.palladiosimulator.pcm.parameter.VariableCharacterisationType;
+import org.palladiosimulator.pcm.protocol.ProtocolFactory;
+import org.palladiosimulator.pcm.protocol.ProtocolPackage;
+import org.palladiosimulator.pcm.qosannotations.QosannotationsFactory;
+import org.palladiosimulator.pcm.qosannotations.QosannotationsPackage;
+import org.palladiosimulator.pcm.qosannotations.qos_performance.QosPerformanceFactory;
+import org.palladiosimulator.pcm.qosannotations.qos_performance.QosPerformancePackage;
+import org.palladiosimulator.pcm.qosannotations.qos_reliability.QosReliabilityFactory;
+import org.palladiosimulator.pcm.qosannotations.qos_reliability.QosReliabilityPackage;
+import org.palladiosimulator.pcm.reliability.ReliabilityFactory;
+import org.palladiosimulator.pcm.reliability.ReliabilityPackage;
 import org.palladiosimulator.pcm.repository.ComponentType;
 import org.palladiosimulator.pcm.repository.ParameterModifier;
 import org.palladiosimulator.pcm.repository.PrimitiveDataType;
 import org.palladiosimulator.pcm.repository.PrimitiveTypeEnum;
 import org.palladiosimulator.pcm.repository.RepositoryFactory;
 import org.palladiosimulator.pcm.repository.RepositoryPackage;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentPackage;
+import org.palladiosimulator.pcm.resourcetype.ResourcetypeFactory;
+import org.palladiosimulator.pcm.resourcetype.ResourcetypePackage;
+import org.palladiosimulator.pcm.seff.SeffFactory;
+import org.palladiosimulator.pcm.seff.SeffPackage;
+import org.palladiosimulator.pcm.seff.seff_performance.SeffPerformanceFactory;
+import org.palladiosimulator.pcm.seff.seff_performance.SeffPerformancePackage;
+import org.palladiosimulator.pcm.seff.seff_reliability.SeffReliabilityFactory;
+import org.palladiosimulator.pcm.seff.seff_reliability.SeffReliabilityPackage;
+import org.palladiosimulator.pcm.subsystem.SubsystemFactory;
+import org.palladiosimulator.pcm.subsystem.SubsystemPackage;
+import org.palladiosimulator.pcm.system.SystemFactory;
+import org.palladiosimulator.pcm.system.SystemPackage;
+import org.palladiosimulator.pcm.usagemodel.UsagemodelFactory;
+import org.palladiosimulator.pcm.usagemodel.UsagemodelPackage;
 
 /**
  *
@@ -171,25 +207,32 @@ public class GenericComponentProvider<T extends EObject> {
     }
 
     public EObject readComponent(final Node node) {
+        /**
+         * Get the node's data type from its label and instantiate a new empty object of this data
+         * type
+         */
         Label label;
         try (Transaction tx = this.getGraph().beginTx()) {
             label = this.getFirstLabel(node.getLabels());
             tx.success();
         }
-        final EClass eClass = (EClass) RepositoryPackage.eINSTANCE.getEClassifier(label.name());
-        final EObject component = RepositoryFactory.eINSTANCE.create(eClass);
+        System.out.println(label.name());
+
+        final EObject component = this.instantiateEObject(label.name());
 
         /** Iterate over all attributes */
         try (Transaction tx = this.getGraph().beginTx()) {
 
-            // System.out.println("reading " + component);
+            System.out.println("reading " + component);
             for (final EAttribute attr : component.eClass().getEAllAttributes()) {
-                // System.out.print("\tattribute " + attr.getName() + " = ");
+                System.out.print("\tattribute " + attr.getName() + " = ");
                 try {
                     final Object value = this.instantiateAttribute(attr.getEAttributeType().getInstanceClass(),
                             node.getProperty(attr.getName()).toString());
-                    // System.out.println(value);
-                    component.eSet(attr, value);
+                    System.out.println(value);
+                    if (value != null) {
+                        component.eSet(attr, value);
+                    }
                 } catch (final NotFoundException e) {
                     component.eSet(attr, null);
                 }
@@ -200,14 +243,18 @@ public class GenericComponentProvider<T extends EObject> {
         /** Iterate over all references */
         for (final EReference ref : component.eClass().getEAllReferences()) {
             final String refName = ref.getName();
-            // System.out.print("\treference " + refName + " = ");
+            System.out.print("\treference " + refName + " = ");
             Object refReprensation = component.eGet(ref);
 
+            /** Iterate over all outgoing containment relationships of the node */
             try (Transaction tx = this.getGraph().beginTx()) {
                 for (final Relationship rel : node.getRelationships(Direction.OUTGOING, PcmRelationshipType.CONTAINS)) {
+                    /** If a relationship in the graph matches the references name... */
                     if (refName.equals(rel.getProperty(GenericComponentProvider.REF_NAME))) {
+                        System.out.println(rel.getProperty(GenericComponentProvider.REF_NAME));
                         final Node endNode = rel.getEndNode();
 
+                        /** ...recursively create an instance of the referenced object */
                         if (refReprensation instanceof EList<?>) {
                             final EObject endComponent = new GenericComponentProvider<>(this.graph)
                                     .readComponent(endNode);
@@ -308,6 +355,69 @@ public class GenericComponentProvider<T extends EObject> {
             }
         }
 
+        return null;
+    }
+
+    private EObject instantiateEObject(final String name) {
+
+        if (CorePackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) CorePackage.eINSTANCE.getEClassifier(name);
+            return CoreFactory.eINSTANCE.create(eClass);
+        } else if (EntityPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) EntityPackage.eINSTANCE.getEClassifier(name);
+            return EntityFactory.eINSTANCE.create(eClass);
+        } else if (CompositionPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) CompositionPackage.eINSTANCE.getEClassifier(name);
+            return CompositionFactory.eINSTANCE.create(eClass);
+        } else if (UsagemodelPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) UsagemodelPackage.eINSTANCE.getEClassifier(name);
+            return UsagemodelFactory.eINSTANCE.create(eClass);
+        } else if (RepositoryPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) RepositoryPackage.eINSTANCE.getEClassifier(name);
+            return RepositoryFactory.eINSTANCE.create(eClass);
+        } else if (ResourcetypePackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) ResourcetypePackage.eINSTANCE.getEClassifier(name);
+            return ResourcetypeFactory.eINSTANCE.create(eClass);
+        } else if (ProtocolPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) ProtocolPackage.eINSTANCE.getEClassifier(name);
+            return ProtocolFactory.eINSTANCE.create(eClass);
+        } else if (ParameterPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) ParameterPackage.eINSTANCE.getEClassifier(name);
+            return ParameterFactory.eINSTANCE.create(eClass);
+        } else if (ReliabilityPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) ReliabilityPackage.eINSTANCE.getEClassifier(name);
+            return ReliabilityFactory.eINSTANCE.create(eClass);
+        } else if (SeffPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) SeffPackage.eINSTANCE.getEClassifier(name);
+            return SeffFactory.eINSTANCE.create(eClass);
+        } else if (SeffPerformancePackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) SeffPerformancePackage.eINSTANCE.getEClassifier(name);
+            return SeffPerformanceFactory.eINSTANCE.create(eClass);
+        } else if (SeffReliabilityPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) SeffReliabilityPackage.eINSTANCE.getEClassifier(name);
+            return SeffReliabilityFactory.eINSTANCE.create(eClass);
+        } else if (QosannotationsPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) QosannotationsPackage.eINSTANCE.getEClassifier(name);
+            return QosannotationsFactory.eINSTANCE.create(eClass);
+        } else if (QosPerformancePackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) QosPerformancePackage.eINSTANCE.getEClassifier(name);
+            return QosPerformanceFactory.eINSTANCE.create(eClass);
+        } else if (QosReliabilityPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) QosReliabilityPackage.eINSTANCE.getEClassifier(name);
+            return QosReliabilityFactory.eINSTANCE.create(eClass);
+        } else if (SystemPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) SystemPackage.eINSTANCE.getEClassifier(name);
+            return SystemFactory.eINSTANCE.create(eClass);
+        } else if (ResourceenvironmentPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) ResourceenvironmentPackage.eINSTANCE.getEClassifier(name);
+            return ResourceenvironmentFactory.eINSTANCE.create(eClass);
+        } else if (AllocationPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) AllocationPackage.eINSTANCE.getEClassifier(name);
+            return AllocationFactory.eINSTANCE.create(eClass);
+        } else if (SubsystemPackage.eINSTANCE.getEClassifier(name) != null) {
+            final EClass eClass = (EClass) SubsystemPackage.eINSTANCE.getEClassifier(name);
+            return SubsystemFactory.eINSTANCE.create(eClass);
+        }
         return null;
     }
 
--- analysis/src/main/java/org/iobserve/analysis/modelneo4j/genericapproach/TestGeneric.java
+++ analysis/src/main/java/org/iobserve/analysis/modelneo4j/genericapproach/TestGeneric.java
@@ -21,7 +21,6 @@ import org.iobserve.analysis.InitializeModelProviders;
 import org.iobserve.analysis.model.RepositoryModelProvider;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.palladiosimulator.pcm.repository.OperationInterface;
 import org.palladiosimulator.pcm.repository.Repository;
 
 /**
@@ -49,15 +48,20 @@ public class TestGeneric {
         // System.out.println("Writing to db");
         // new GenericComponentProvider<>(graph).createComponent(repository);
 
+        // System.out.println("Reading id -> object from db");
+        // final OperationInterface inter = (OperationInterface) new
+        // GenericComponentProvider<>(graph)
+        // .readComponent("OperationInterface", "_j8RD0NYgEeWrM-HnT5f_ug");
+
         System.out.println("Reading id -> object from db");
-        final OperationInterface inter = (OperationInterface) new GenericComponentProvider<>(graph)
-                .readComponent("OperationInterface", "_j8RD0NYgEeWrM-HnT5f_ug");
+        final Repository repository2 = (Repository) new GenericComponentProvider<>(graph).readComponent("Repository",
+                repository.getId());
 
         final GraphDatabaseService graph2 = new GraphDatabaseFactory().newEmbeddedDatabase(TestGeneric.DB_PATH2);
         TestGeneric.registerShutdownHook(graph2);
 
         System.out.println("Writing to db2");
-        new GenericComponentProvider<>(graph2).createComponent(inter);
+        new GenericComponentProvider<>(graph2).createComponent(repository2);
 
         // System.out.println("Reading type -> ids from db");
         // final List<String> ids = new
