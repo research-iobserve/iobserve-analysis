b481116a7c8a2dd700a5e9c932703f79dcb83bda
tpoeppke <t.poeppke@gmx.de>
2017-06-01 22:52:42 +0200

    Upgraded build dependencies and regenerated systemadaptation

--- planning.systemadaptation/src/main/java/org/iobserve/planning/systemadaptation/DeallocateAction.java
+++ planning.systemadaptation/src/main/java/org/iobserve/planning/systemadaptation/DeallocateAction.java
@@ -4,7 +4,6 @@ package org.iobserve.planning.systemadaptation;
 
 import org.palladiosimulator.pcm.allocation.AllocationContext;
 
-
 /**
  * <!-- begin-user-doc -->
  * A representation of the model object '<em><b>Deallocate Action</b></em>'.
@@ -22,7 +21,6 @@ import org.palladiosimulator.pcm.allocation.AllocationContext;
  * @generated
  */
 public interface DeallocateAction extends AssemblyContextAction {
-
 	/**
 	 * Returns the value of the '<em><b>Old Allocation Context</b></em>' reference.
 	 * <!-- begin-user-doc -->
@@ -48,4 +46,5 @@ public interface DeallocateAction extends AssemblyContextAction {
 	 * @generated
 	 */
 	void setOldAllocationContext(AllocationContext value);
+
 } // DeallocateAction
--- planning.systemadaptation/src/main/java/org/iobserve/planning/systemadaptation/impl/DeallocateActionImpl.java
+++ planning.systemadaptation/src/main/java/org/iobserve/planning/systemadaptation/impl/DeallocateActionImpl.java
@@ -6,6 +6,7 @@ import org.eclipse.emf.ecore.EClass;
 
 import org.iobserve.planning.systemadaptation.DeallocateAction;
 import org.iobserve.planning.systemadaptation.systemadaptationPackage;
+
 import org.palladiosimulator.pcm.allocation.AllocationContext;
 
 /**
