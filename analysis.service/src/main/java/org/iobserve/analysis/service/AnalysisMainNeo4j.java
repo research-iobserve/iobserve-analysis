8f5de6b454200e471f7b7f7d6d791ab92d01c3d1
Lars Blümke <lbl@informatik.uni-kiel.de>
2017-06-23 15:28:24 +0200

    added modified analysis main and a graph loader to get a neo4j graph from to database folder

---
+++ analysis.service/src/main/java/org/iobserve/analysis/service/AnalysisMainNeo4j.java
@@ -0,0 +1,209 @@
+/***************************************************************************
+ * Copyright (C) 2014 iObserve Project (https://www.iobserve-devops.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package org.iobserve.analysis.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.iobserve.analysis.InitializeModelProviders;
+import org.iobserve.analysis.model.AllocationModelProvider;
+import org.iobserve.analysis.model.RepositoryModelProvider;
+import org.iobserve.analysis.model.ResourceEnvironmentModelProvider;
+import org.iobserve.analysis.model.SystemModelProvider;
+import org.iobserve.analysis.model.UsageModelProvider;
+import org.iobserve.analysis.model.correspondence.ICorrespondence;
+import org.iobserve.analysis.modelneo4j.GraphLoader;
+import org.iobserve.analysis.modelneo4j.ModelProvider;
+import org.iobserve.analysis.utils.ExecutionTimeLogger;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.palladiosimulator.pcm.repository.OperationInterface;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.converters.FileConverter;
+import com.beust.jcommander.converters.IntegerConverter;
+
+import teetime.framework.Configuration;
+import teetime.framework.Execution;
+
+/**
+ * Main class for starting the iObserve application.
+ *
+ * @author Reiner Jung
+ * @author Robert Heinrich
+ * @author Alessandro Giusa
+ */
+public final class AnalysisMainNeo4j {
+
+    @Parameter(names = "--help", help = true)
+    private boolean help;
+
+    @Parameter(names = { "-v",
+            "--variance-of-user-groups" }, required = true, description = "Variance of user groups.", converter = IntegerConverter.class)
+    private int varianceOfUserGroups;
+
+    @Parameter(names = { "-t",
+            "--think-time" }, required = true, description = "Think time.", converter = IntegerConverter.class)
+    private int thinkTime;
+
+    @Parameter(names = { "-c",
+            "--closed-workload" }, required = false, description = "Closed workload.", converter = IntegerConverter.class)
+    private boolean closedWorkload;
+
+    @Parameter(names = { "-i",
+            "--input" }, required = true, description = "port number to listen for new connections of Kieker writers.", converter = IntegerConverter.class)
+    private int listenPort;
+
+    @Parameter(names = { "-o",
+            "--output" }, required = true, description = "hostname and port of the iobserve visualization, e.g., visualization:80.")
+    private String output;
+
+    @Parameter(names = { "-s", "--system" }, required = true, description = "system id.")
+    private String systemId;
+
+    @Parameter(names = { "-p",
+            "--pcm" }, required = true, description = "Directory containing PCM model data.", converter = FileConverter.class)
+    private File pcmModelsDirectory;
+
+    @Parameter(names = { "-pn4j",
+            "--pcmneo4j" }, required = true, description = "Directory containing Neo4j database with PCM model data.", converter = FileConverter.class)
+    private File pcmModelsNeo4jDirectory;
+
+    /**
+     * Default constructor.
+     */
+    private AnalysisMainNeo4j() {
+        // do nothing here
+    }
+
+    /**
+     * Main function.
+     *
+     * @param args
+     *            command line arguments.
+     */
+    public static void main(final String[] args) {
+        final AnalysisMainNeo4j main = new AnalysisMainNeo4j();
+        final JCommander commander = new JCommander(main);
+        try {
+            commander.parse(args);
+            main.execute(commander);
+        } catch (final ParameterException e) {
+            System.err.println(e.getLocalizedMessage());
+            commander.usage();
+        } catch (final IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            commander.usage();
+        }
+    }
+
+    private void execute(final JCommander commander) throws IOException {
+        if (this.help) {
+            commander.usage();
+            System.exit(1);
+        } else {
+            this.checkDirectory(this.pcmModelsDirectory, "Palladio Model", commander);
+            this.checkDirectory(this.pcmModelsNeo4jDirectory, "Palladio Model Neo4j", commander);
+            /** process parameter. */
+
+            final String[] outputs = this.output.split(":");
+            if (outputs.length == 2) {
+                final String outputHostname = outputs[0];
+                final String outputPort = outputs[1];
+
+                /** process parameter. */
+                final InitializeModelProviders modelProvider = new InitializeModelProviders(this.pcmModelsDirectory);
+
+                final ICorrespondence correspondenceModel = modelProvider.getCorrespondenceModel();
+                final UsageModelProvider usageModelProvider = modelProvider.getUsageModelProvider();
+                final RepositoryModelProvider repositoryModelProvider = modelProvider.getRepositoryModelProvider();
+                final ResourceEnvironmentModelProvider resourceEvnironmentModelProvider = modelProvider
+                        .getResourceEnvironmentModelProvider();
+                final AllocationModelProvider allocationModelProvider = modelProvider.getAllocationModelProvider();
+                final SystemModelProvider systemModelProvider = modelProvider.getSystemModelProvider();
+
+                final Configuration configuration = new ServiceConfiguration(this.listenPort, outputHostname,
+                        outputPort, this.systemId, this.varianceOfUserGroups, this.thinkTime, this.closedWorkload,
+                        correspondenceModel, usageModelProvider, repositoryModelProvider,
+                        resourceEvnironmentModelProvider, allocationModelProvider, systemModelProvider);
+
+                /** Neo4j database ************************************************************/
+                // Create a graph loader to receive the different neo4j graphs for each model
+                final GraphLoader graphLoader = new GraphLoader(this.pcmModelsNeo4jDirectory);
+
+                // If you use the database for the very first time the graphs will be empty. You can
+                // initialize the graph with a model from an old provider. Note: The initialization
+                // overwrites possibly existing models, so you want to do this just on the very
+                // first setup or if you want to return to clean test data in the graph.
+                graphLoader.initializeAllocationModelGraph(allocationModelProvider.getModel());
+                System.out.println("Initialized allocation model graph");
+                graphLoader.initializeRepositoryModelGraph(repositoryModelProvider.getModel());
+                System.out.println("Initialized repository model graph");
+                graphLoader.initializeResourceEnvironmentModelGraph(resourceEvnironmentModelProvider.getModel());
+                System.out.println("Initialized resource environment model graph");
+                graphLoader.initializeSystemModelGraph(systemModelProvider.getModel());
+                System.out.println("Initialized system model graph");
+                graphLoader.initializeUsageModelGraph(usageModelProvider.getModel());
+                System.out.println("Initialized usage model graph");
+
+                // Instead of passing the old providers to the analysis you can now pass the graph
+                // of the model you want to use...
+                final GraphDatabaseService allocationModelGraph = graphLoader.getAllocationModelGraph();
+                System.out.println("Loaded allocation model graph");
+                final GraphDatabaseService repositoryModelGraph = graphLoader.getRepositoryModelGraph();
+                System.out.println("Loaded repository model graph");
+                final GraphDatabaseService resourceEnvironmentModelGraph = graphLoader
+                        .getResourceEnvironmentModelGraph();
+                System.out.println("Loaded resource environment model graph");
+                final GraphDatabaseService systemModelGraph = graphLoader.getSystemModelGraph();
+                System.out.println("Loaded system model graph");
+                final GraphDatabaseService usageModelGraph = graphLoader.getUsageModelGraph();
+                System.out.println("Loaded usage model graph");
+
+                // You can access it with a model provider, for example
+                final String idOfInterfaceIWant = repositoryModelProvider.getModel().getInterfaces__Repository().get(0)
+                        .getId();
+                final OperationInterface opInter = new ModelProvider<OperationInterface>(repositoryModelGraph)
+                        .readComponent(OperationInterface.class, idOfInterfaceIWant);
+                /******************************************************************************/
+
+                System.out.println("Analysis configuration");
+                final Execution<Configuration> analysis = new Execution<>(configuration);
+                System.out.println("Analysis start");
+                analysis.executeBlocking();
+                System.out.println("Anaylsis complete");
+                ExecutionTimeLogger.getInstance().exportAsCsv();
+            }
+        }
+    }
+
+    private void checkDirectory(final File location, final String locationLabel, final JCommander commander)
+            throws IOException {
+        if (!location.exists()) {
+            System.err.println(locationLabel + " path " + location.getCanonicalPath() + " does not exist.");
+            commander.usage();
+            System.exit(1);
+        }
+        if (!location.isDirectory()) {
+            System.err.println(locationLabel + " path " + location.getCanonicalPath() + " is not a directory.");
+            commander.usage();
+            System.exit(1);
+        }
+
+    }
+
+}
