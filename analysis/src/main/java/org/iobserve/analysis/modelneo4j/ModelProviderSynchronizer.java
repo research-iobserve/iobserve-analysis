97d0aff6fa6bf6407906c2f2a2cf1f4fff07d5c6
Lars Blümke <lbl@informatik.uni-kiel.de>
2017-07-07 10:46:51 +0200

    implemented a class for synchronization of database access

---
+++ analysis/src/main/java/org/iobserve/analysis/modelneo4j/ModelProviderSynchronizer.java
@@ -0,0 +1,70 @@
+/***************************************************************************
+ * Copyright (C) 2017 iObserve Project (https://www.iobserve-devops.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+package org.iobserve.analysis.modelneo4j;
+
+import java.util.HashMap;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Can be used to synchronize access to a graph database to prevent scenarios where two threads read
+ * a model from the same graph database, edit the model independently and then both write back to
+ * the database ignoring the possible changes made by the other thread.
+ *
+ * @author Lars Bluemke
+ *
+ */
+public class ModelProviderSynchronizer {
+
+    private static final Logger LOGGER = LogManager.getLogger(TestGeneric.class);
+    private static HashMap<Graph, ModelProvider<?>> locks = new HashMap<>();
+
+    /**
+     * Gets the lock for the graph database of the calling {@link ModelProvider}. Blocks if the lock
+     * is currently hold by another provider.
+     *
+     * @param modelProvider
+     *            The model provider trying for the lock
+     * @throws InterruptedException
+     *             If any thread interrupted the current thread before or while the current thread
+     *             was waiting for a notification
+     */
+    public static synchronized void getLock(final ModelProvider<?> modelProvider) throws InterruptedException {
+        final Graph graph = modelProvider.getGraph();
+        while ((ModelProviderSynchronizer.locks.get(graph) != null)
+                && (ModelProviderSynchronizer.locks.get(graph) != modelProvider)) {
+            graph.wait();
+        }
+        ModelProviderSynchronizer.locks.put(graph, modelProvider);
+    }
+
+    /**
+     * Releases the lock for the graph database of the calling {@link ModelProvider}.
+     *
+     * @param modelProvider
+     *            The model provider trying to release the lock
+     */
+    public static synchronized void releaseLock(final ModelProvider<?> modelProvider) {
+        final Graph graph = modelProvider.getGraph();
+        if (ModelProviderSynchronizer.locks.get(graph) == modelProvider) {
+            ModelProviderSynchronizer.locks.remove(graph);
+            graph.notify();
+        } else {
+            ModelProviderSynchronizer.LOGGER.warn("Cannot release the lock - you are not holding it.");
+        }
+    }
+}
