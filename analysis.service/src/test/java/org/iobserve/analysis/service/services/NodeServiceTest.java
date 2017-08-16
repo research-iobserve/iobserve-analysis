4a898085e0a64464e74c1471d7100ec4f86096b2
jweg <jweg@informatik.uni-kiel.de>
2017-08-16 13:03:14 +0200

    package structure for tests created

---
+++ analysis.service/src/test/java/org/iobserve/analysis/service/services/NodeServiceTest.java
@@ -0,0 +1,67 @@
+package org.iobserve.analysis.service.services;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
+import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
+
+/**
+ * Tests for {@link NodeService}
+ *
+ * @author jweg
+ *
+ */
+public class NodeServiceTest {
+
+    /** class under test */
+    private NodeService nodeService;
+
+    /** test data */
+    private ResourceContainer testResourceContainer;
+    private String testSystemId;
+    private String testNodegroupId;
+
+    /** expected result */
+    private JsonObject expectedNode;
+
+    /**
+     * Prepares test data.
+     */
+    @Before
+    public void setup() {
+
+        this.nodeService = new NodeService();
+
+        /** test data */
+        this.testResourceContainer = ResourceenvironmentFactory.eINSTANCE.createResourceContainer();
+        this.testResourceContainer.setId("test_id");
+        this.testResourceContainer.setEntityName("test_name");
+
+        this.testSystemId = "test_systemId";
+        this.testNodegroupId = "test_nodegroupId";
+
+        /** expected result */
+        this.expectedNode = Json.createObjectBuilder().add("type", "node").add("id", "test_id")
+                .add("systemId", this.testSystemId).add("nodeGroupId", this.testNodegroupId)
+                .add("hostname", "test_name").build();
+
+    }
+
+    /**
+     * Checks whether {@link NodeService#createNode(ResourceContainer,String,String)} works as
+     * expected.
+     */
+    @Test
+    public void checkCreateNode() {
+
+        final JsonObject actualNode = this.nodeService.createNode(this.testResourceContainer, this.testSystemId,
+                this.testNodegroupId);
+
+        Assert.assertEquals(this.expectedNode, actualNode);
+    }
+
+}
--- analysis.service/src/test/java/org/iobserve/analysis/service/updater/AllocationVisualizationStageTest.java
+++ analysis.service/src/test/java/org/iobserve/analysis/service/updater/AllocationVisualizationStageTest.java
@@ -9,15 +9,19 @@ import java.util.List;
 import org.iobserve.analysis.modelneo4j.ModelProvider;
 import org.iobserve.common.record.ContainerAllocationEvent;
 import org.iobserve.common.record.IAllocationRecord;
+import org.iobserve.models.Changelog;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceenvironmentFactory;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
 
 import teetime.framework.test.StageTester;
@@ -36,9 +40,9 @@ public class AllocationVisualizationStageTest {
     private AllocationVisualizationStage allocationVisualizationStage;
     @Mock
     private ModelProvider<ResourceContainer> mockedResourceContainerModelProvider;
-    private final String systemId = "testSystemId";
+    private static final String systemId = "testSystemId";
     private final String outputPort = "9090";
-    private final String outputHostname = "localhost";
+    private static final String outputHostname = "localhost";
     private URL changelogURL;
 
     /** list of test resource container */
@@ -58,20 +62,40 @@ public class AllocationVisualizationStageTest {
     /** input events */
     private final List<IAllocationRecord> inputEvents = new ArrayList<>();
 
+    private TestHandler testHandler;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private Changelog changelogRequest;
+
+    // /** data for mocking SendHttpRequest */
+    // private static final String TEST_RESPONSE = "test\nresponse";
+    // private static final String nodeId = "test_nodeId";
+    // private static final String nodegroupId = "test_nodegroupId";
+    // private final String NODE_DATA = Json.createObjectBuilder().add("type", "node")
+    // .add("id", AllocationVisualizationStageTest.nodeId)
+    // .add("systemId", AllocationVisualizationStageTest.systemId)
+    // .add("nodeGroupId", AllocationVisualizationStageTest.nodegroupId)
+    // .add("hostname", AllocationVisualizationStageTest.outputHostname).build();
+
+    // private static final JsonObject NODEGROUP_DATA = Json.createObjectBuilder().add("type",
+    // "nodeGroup")
+    // .add("id", AllocationVisualizationStageTest.nodegroupId)
+    // .add("systemId", AllocationVisualizationStageTest.systemId).add("name",
+    // "nodeGroupName").build();
+    // private static final String PROTOCOL = "http";
+    //
     /**
      * -initializing of stage under test and events for input -http server that takes HTTP-Requests
      *
-     * @throws IOException
+     * @throws Exception
      */
     @Before
-    public void initServer() throws IOException {
-        System.out.println("@Before am Anfang");
+    public void initServer() throws Exception {
 
-        this.changelogURL = new URL("http://" + this.outputHostname + ":" + this.outputPort + "/v1/systems/"
-                + this.systemId + "/changelogs");
+        this.changelogURL = new URL("http://" + AllocationVisualizationStageTest.outputHostname + ":" + this.outputPort
+                + "/v1/systems/" + AllocationVisualizationStageTest.systemId + "/changelogs");
 
-        this.allocationVisualizationStage = new AllocationVisualizationStage(this.changelogURL, this.systemId,
-                this.mockedResourceContainerModelProvider);
+        this.allocationVisualizationStage = new AllocationVisualizationStage(this.changelogURL,
+                AllocationVisualizationStageTest.systemId, this.mockedResourceContainerModelProvider);
 
         /** test event */
         this.allocationEvent = new ContainerAllocationEvent(AllocationVisualizationStageTest.URL);
@@ -81,27 +105,40 @@ public class AllocationVisualizationStageTest {
 
         /** list of test resource container */
         this.testResourceContainer = ResourceenvironmentFactory.eINSTANCE.createResourceContainer();
+        this.testResourceContainer.setEntityName("test_nodeName");
+        this.testResourceContainer.setId("test_nodeId");
         this.testResourceContainerList.add(this.testResourceContainer);
 
         // stubbing
         Mockito.when(this.mockedResourceContainerModelProvider.readOnlyComponentByName(ResourceContainer.class,
                 AllocationVisualizationStageTest.SERVICE)).thenReturn(this.testResourceContainerList);
+        this.testHandler = new TestHandler();
 
-        System.out.println("@Before setup and start server");
         final HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
-        server.createContext("/v1/systems/testSystemId/changelogs", new TestHandler());
+        server.createContext("/v1/systems/testSystemId/changelogs", this.testHandler);
         server.start();
+
     }
 
     /**
      * Check information in HTTP request
+     *
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonParseException
      */
     @Test
-    public void test() {
-        System.out.println("@Test");
+    public void test() throws JsonParseException, JsonMappingException, IOException {
+
         // Sende container allocation event und überprüfe, ob das richtige drinsteht
         StageTester.test(this.allocationVisualizationStage).and().send(this.inputEvents)
                 .to(this.allocationVisualizationStage.getInputPort()).start();
+
+        this.changelogRequest = this.objectMapper.readValue(this.testHandler.getRequestBody(), Changelog.class);
+
+        System.out.printf("jsonObject changelogRequest:%s\n", this.changelogRequest.toString());
+
+        // Assert.assertArrayEquals(expecteds, this.testHandler.getRequestBody());
 
     }
 
