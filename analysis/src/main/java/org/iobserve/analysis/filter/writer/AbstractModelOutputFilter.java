9e3213efa833f50afaf9c5fc8ed29a2e2c938678
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-08-27 23:02:59 +0200

    added missing file

---
+++ analysis/src/main/java/org/iobserve/analysis/filter/writer/AbstractModelOutputFilter.java
@@ -0,0 +1,9 @@
+package org.iobserve.analysis.filter.writer;
+
+import org.iobserve.analysis.cdoruserbehavior.filter.models.BehaviorModel;
+
+import teetime.framework.AbstractConsumerStage;
+
+abstract public class AbstractModelOutputFilter extends AbstractConsumerStage<BehaviorModel> {
+
+}
