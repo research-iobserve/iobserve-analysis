c40f7c52ba57b6f393d85aa7b6a8764409c4eb03
madcau <mad@informatik.uni-kiel.de>
2017-12-06 13:58:51 +0100

    Update add_jenkins_dependencies.sh

--- .jenkinsfile/add_jenkins_dependencies.sh
+++ .jenkinsfile/add_jenkins_dependencies.sh
@@ -1,7 +1,18 @@
-git clone https://github.com/research-iobserve/iobserve-repository.git
-path="$(pwd -P )"
-path=$path"/iobserve-repository/mvn-repo/"
-echo "Repo at "$path
-cd $old_path
-file_entry="api.baseline="$path
-echo $file_entry >"gradle.properties"
+#!/bin/sh
+
+repo_path="https://github.com/research-iobserve/iobserve-repository.git"
+
+clone_and_add_path() {
+  old_path="$(pwd -P )"
+  echo "Path to analysis "$old_path
+  git clone $repo_path
+  path="$(pwd -P )"
+  path=$path"/iobserve-repository/mvn-repo/"	
+  echo "Repo at "$path
+  cd $old_path
+  file_entry="api.baseline="$path
+  echo $file_entry >"gradle.properties"
+  echo "added/overwrote file 'gradle.properties' and added repo path" 	
+}
+
+clone_and_add_path
