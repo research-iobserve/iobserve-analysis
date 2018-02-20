790edbb269190847048618e3fcfb84d63cce3198
jannisk <janniskuckei@gmx.de>
2018-02-20 14:00:26 +0100

    Created strategy interface for model generation IModelGenerationStrategy, implemented in UnionModelGenerationStrategy, made TModelGeneration use interface

--- analysis/src/main/java/org/iobserve/analysis/clustering/filter/TModelGeneration.java
+++ analysis/src/main/java/org/iobserve/analysis/clustering/filter/TModelGeneration.java
@@ -1,12 +1,6 @@
 package org.iobserve.analysis.clustering.filter;
 
-import java.util.Iterator;
-import java.util.List;
-
 import org.iobserve.analysis.clustering.filter.models.BehaviorModel;
-import org.iobserve.analysis.clustering.filter.models.BehaviorModelTable;
-import org.iobserve.analysis.clustering.filter.models.EntryCallEdge;
-import org.iobserve.analysis.clustering.filter.models.EntryCallNode;
 
 import teetime.framework.AbstractStage;
 import teetime.framework.InputPort;
@@ -15,70 +9,45 @@ import teetime.framework.OutputPort;
 //<BehaviorModelTable[],List<List<Integer>>>
 public class TModelGeneration extends AbstractStage {
     private final OutputPort<BehaviorModel[]> outputPort = this.createOutputPort();
-    private final InputPort<BehaviorModel[]> inputTables = this.createInputPort(BehaviorModelTable[].class);
-    private final InputPort<Integer[][]> inputGroups = this.createInputPort();
+    private final InputPort<BehaviorModel[]> modelsInputPort = this.createInputPort(BehaviorModel[].class);
+    private final InputPort<Integer[][]> groupsInputPort = this.createInputPort();
 
-    private BehaviorModelTable[] tables;
+    private final IModelGenerationStrategy modelGenerationStrategy;
+    private BehaviorModel[] models;
     private Integer[][] groups;
+
+    public TModelGeneration(final IModelGenerationStrategy modelGenerationStrategy) {
+        this.modelGenerationStrategy = modelGenerationStrategy;
+    }
 
     @Override
     protected void execute() {
-        if (this.tables == null) {
-            this.tables = this.inputTables.receive();
-        }
+        this.models = this.modelsInputPort.receive();
+        this.groups = this.groupsInputPort.receive();
 
-        if (this.groups == null) {
-            this.groups = this.inputGroups.receive();
-        }
-
-        if ((this.tables == null) || (this.groups == null)) {
+        if ((this.models == null) || (this.groups == null)) {
             return;
         }
 
-        final BehaviorModel[] models = new BehaviorModel[this.groups.size()];
+        final BehaviorModel[] representativeModels = new BehaviorModel[this.groups.length];
 
-        final Iterator<List<Integer>> iteratorGroup = this.groups.iterator();
-        for (final BehaviorModel model : models) {
-            this.generateModel(iteratorGroup.next(), model);
-        }
-
-        this.outputPort.send(models);
-    }
-
-    // TODO: parameters...
-    private void generateModel(final List<Integer> group, BehaviorModel model) {
-        // Generate EntryCallNodes
-        BehaviorModelTable table = this.tables[group.get(0)];
-        final String[] signatures = table.getInverseSignatures(); // TODO: add this to table class
-        final EntryCallNode[] nodes = new EntryCallNode[signatures.length];
-
-        for (int i = 0; i < signatures.length; i++) {
-            nodes[i] = new EntryCallNode(signatures[i]);
-            model.addNode(nodes[i]);
-        }
-
-        // Initialize edge counter
-        final int[][] edges = new int[signatures.length][signatures.length];
-
-        model = new BehaviorModel();
-        for (final Integer index : group) {
-            table = this.tables[index];
-
-            final Integer[][] transitions = table.getTransitions();
-            for (int i = 0; i < signatures.length; i++) {
-                for (int j = 0; j < signatures.length; j++) {
-                    edges[i][j] += transitions[i][j];
-                }
+        for (int i = 0; i < this.groups.length; i++) {
+            /** Put all models from this group into an array */
+            final BehaviorModel[] groupModels = new BehaviorModel[this.groups[i].length];
+            for (int j = 0; j < groupModels.length; j++) {
+                groupModels[j] = this.models[this.groups[i][j]];
             }
+
+            /** Find representative model with strategy */
+            representativeModels[i] = this.modelGenerationStrategy.generateModel(groupModels);
+            representativeModels[i].setName("Representative Model of group #" + i);
         }
 
-        for (int i = 0; i < signatures.length; i++) {
-            for (int j = 0; j < signatures.length; j++) {
-                final EntryCallEdge edge = new EntryCallEdge(nodes[i], nodes[j], edges[i][j]); // TODO: Check order of
-                                                                                               // args
-                model.addEdge(edge);
-            }
-        }
+        this.outputPort.send(representativeModels);
+
+        /** Clear state */
+        this.models = null;
+        this.groups = null;
     }
 
     public OutputPort<BehaviorModel[]> getOutputPort() {
