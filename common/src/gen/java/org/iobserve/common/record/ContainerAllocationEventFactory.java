efb27034fcdd62c512550811e505eb8905fa481f
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-03-14 11:14:56 +0100

    added package-infos and regenerated records

--- common/src/gen/java/org/iobserve/common/record/ContainerAllocationEventFactory.java
+++ common/src/gen/java/org/iobserve/common/record/ContainerAllocationEventFactory.java
@@ -1,11 +1,11 @@
 /***************************************************************************
- * Copyright 2018 iObserve Project (http://www.iobserve-devops.net)
+ * Copyright 2018 iObserve Project (http://iobserve-devops.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,30 +15,31 @@
  ***************************************************************************/
 package org.iobserve.common.record;
 
+
 import kieker.common.exception.RecordInstantiationException;
 import kieker.common.record.factory.IRecordFactory;
 import kieker.common.record.io.IValueDeserializer;
 
 /**
  * @author Reiner Jung
- *
+ * 
  * @since 0.0.2
  */
 public final class ContainerAllocationEventFactory implements IRecordFactory<ContainerAllocationEvent> {
-
-    @Override
-    public ContainerAllocationEvent create(final IValueDeserializer deserializer) throws RecordInstantiationException {
-        return new ContainerAllocationEvent(deserializer);
-    }
-
-    @Override
-    @Deprecated
-    public ContainerAllocationEvent create(final Object[] values) {
-        return new ContainerAllocationEvent(values);
-    }
-
-    @Override
-    public int getRecordSizeInBytes() {
-        return ContainerAllocationEvent.SIZE;
-    }
+	
+	
+	@Override
+	public ContainerAllocationEvent create(final IValueDeserializer deserializer) throws RecordInstantiationException {
+		return new ContainerAllocationEvent(deserializer);
+	}
+	
+	@Override
+	@Deprecated
+	public ContainerAllocationEvent create(final Object[] values) {
+		return new ContainerAllocationEvent(values);
+	}
+	
+	public int getRecordSizeInBytes() {
+		return ContainerAllocationEvent.SIZE;
+	}
 }
