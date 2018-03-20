daf5f7e003822e3cee2b16f7f1e613d3b9799f38
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-03-20 14:36:52 +0100

    minor fixes to reduce checkstyle errors.

--- analysis/src/main/java/teetime/stage/trace/traceReconstruction/TraceReconstructionFilter.java
+++ analysis/src/main/java/teetime/stage/trace/traceReconstruction/TraceReconstructionFilter.java
@@ -1,18 +1,18 @@
-/**
- * Copyright (C) 2015 Christian Wulf, Nelson Tavares de Sousa (http://teetime-framework.github.io)
+/***************************************************************************
+ * Copyright (C) 2018 iObserve Project (https://www.iobserve-devops.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ ***************************************************************************/
 package teetime.stage.trace.traceReconstruction;
 
 import java.util.concurrent.TimeUnit;
@@ -61,7 +61,7 @@ public class TraceReconstructionFilter extends AbstractConsumerStage<IFlowRecord
 
 	@Override
 	public void sendTraceBuffer(final EventBasedTrace traceBufferList) {
-		OutputPort<EventBasedTrace> outputPort = (traceBufferList.isInvalid()) ? this.traceInvalidOutputPort : this.traceValidOutputPort;
+		final OutputPort<EventBasedTrace> outputPort = (traceBufferList.isInvalid()) ? this.traceInvalidOutputPort : this.traceValidOutputPort;
 		outputPort.send(traceBufferList);
 	}
 
