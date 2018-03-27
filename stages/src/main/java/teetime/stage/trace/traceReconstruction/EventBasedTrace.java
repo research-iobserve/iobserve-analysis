7fc32af4c639a878a7dd23ca7b4e83d07584e971
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-03-27 18:05:31 +0200

    fixed comments

--- stages/src/main/java/teetime/stage/trace/traceReconstruction/EventBasedTrace.java
+++ stages/src/main/java/teetime/stage/trace/traceReconstruction/EventBasedTrace.java
@@ -18,15 +18,15 @@ package teetime.stage.trace.traceReconstruction;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import kieker.analysis.plugin.filter.flow.TraceEventRecords;
 import kieker.common.record.flow.trace.AbstractTraceEvent;
 import kieker.common.record.flow.trace.TraceMetadata;
 import kieker.common.record.flow.trace.operation.AfterOperationEvent;
 import kieker.common.record.flow.trace.operation.AfterOperationFailedEvent;
 import kieker.common.record.flow.trace.operation.BeforeOperationEvent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The TraceBuffer is synchronized to prevent problems with concurrent access.
@@ -35,106 +35,114 @@ import kieker.common.record.flow.trace.operation.BeforeOperationEvent;
  */
 public final class EventBasedTrace {
 
-	private static final Logger LOG = LoggerFactory.getLogger(EventBasedTrace.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventBasedTrace.class);
 
-	private final List<AbstractTraceEvent> events = new ArrayList<AbstractTraceEvent>(128);
+    private final List<AbstractTraceEvent> events = new ArrayList<>(128);
 
-	private TraceMetadata traceMetaData;
-	private long traceId = -1;
+    private TraceMetadata traceMetaData;
+    private long traceId = -1;
 
-	private boolean closeable;
-	private boolean damaged;
-	private int openEvents;
-	private int maxOrderIndex = -1;
+    private boolean closeable;
+    private boolean damaged;
+    private int openEvents;
+    private int maxOrderIndex = -1;
 
-	/**
-	 * Creates a new instance of this class.
-	 */
-	public EventBasedTrace() {
-		// default empty constructor
-	}
+    /**
+     * Creates a new instance of this class.
+     */
+    public EventBasedTrace() {
+        // default empty constructor
+    }
 
-	public void insertEvent(final AbstractTraceEvent event) {
-		final long myTraceId = event.getTraceId();
-		synchronized (this) {
-			if (this.traceId == -1) {
-				this.traceId = myTraceId;
-			} else if (this.traceId != myTraceId) {
-				LOG.error("Invalid traceId! Expected: " + this.traceId + " but found: " + myTraceId + " in event " + event.toString());
-				this.damaged = true;
-			}
+    public void insertEvent(final AbstractTraceEvent event) {
+        final long myTraceId = event.getTraceId();
+        synchronized (this) {
+            if (this.traceId == -1) {
+                this.traceId = myTraceId;
+            } else if (this.traceId != myTraceId) {
+                EventBasedTrace.LOGGER.error("Invalid traceId! Expected: " + this.traceId + " but found: " + myTraceId
+                        + " in event " + event.toString());
+                this.damaged = true;
+            }
 
-			final int orderIndex = event.getOrderIndex();
-			if (orderIndex > this.maxOrderIndex) {
-				this.maxOrderIndex = orderIndex;
-			}
-			if (event instanceof BeforeOperationEvent) {
-				if (orderIndex == 0) {
-					this.closeable = true;
-				}
-				this.openEvents++;
-			} else if (event instanceof AfterOperationEvent) {
-				this.openEvents--;
-			} else if (event instanceof AfterOperationFailedEvent) {
-				this.openEvents--;
-			}
+            final int orderIndex = event.getOrderIndex();
+            if (orderIndex > this.maxOrderIndex) {
+                this.maxOrderIndex = orderIndex;
+            }
+            if (event instanceof BeforeOperationEvent) {
+                if (orderIndex == 0) {
+                    this.closeable = true;
+                }
+                this.openEvents++;
+            } else if (event instanceof AfterOperationEvent) {
+                this.openEvents--;
+            } else if (event instanceof AfterOperationFailedEvent) {
+                this.openEvents--;
+            }
 
-			this.events.add(event);
-		}
-	}
+            this.events.add(event);
+        }
+    }
 
-	public void setTrace(final TraceMetadata traceMetaData) {
-		final long myTraceId = traceMetaData.getTraceId();
-		synchronized (this) {
-			if (this.traceId == -1) {
-				this.traceId = myTraceId;
-			} else if (this.traceId != myTraceId) {
-				LOG.error("Invalid traceId! Expected: " + this.traceId + " but found: " + myTraceId + " in trace " + traceMetaData.toString());
-				this.damaged = true;
-			}
+    public void setTrace(final TraceMetadata traceMetaData) {
+        final long myTraceId = traceMetaData.getTraceId();
+        synchronized (this) {
+            if (this.traceId == -1) {
+                this.traceId = myTraceId;
+            } else if (this.traceId != myTraceId) {
+                EventBasedTrace.LOGGER.error("Invalid traceId! Expected: " + this.traceId + " but found: " + myTraceId
+                        + " in trace " + traceMetaData.toString());
+                this.damaged = true;
+            }
 
-			if (this.traceMetaData == null) {
-				this.traceMetaData = traceMetaData;
-			} else {
-				LOG.error("Duplicate Trace entry for traceId " + myTraceId);
-				this.damaged = true;
-			}
-		}
-	}
+            if (this.traceMetaData == null) {
+                this.traceMetaData = traceMetaData;
+            } else {
+                EventBasedTrace.LOGGER.error("Duplicate Trace entry for traceId " + myTraceId);
+                this.damaged = true;
+            }
+        }
+    }
 
-	public boolean isFinished() {
-		synchronized (this) {
-			return this.closeable && !this.isInvalid();
-		}
-	}
+    public boolean isFinished() {
+        synchronized (this) {
+            return this.closeable && !this.isInvalid();
+        }
+    }
 
-	public boolean isInvalid() {
-		synchronized (this) {
-			return (this.traceMetaData == null) || this.damaged || (this.openEvents != 0)
-					|| (((this.maxOrderIndex + 1) != this.events.size()) || this.events.isEmpty());
-		}
-	}
+    /**
+     * Indicates if the trace is invalid, i.e., damaged in some way.
+     *
+     * @return true for invalid traces
+     */
+    public boolean isInvalid() {
+        synchronized (this) {
+            return this.traceMetaData == null || this.damaged || this.openEvents != 0
+                    || this.maxOrderIndex + 1 != this.events.size() || this.events.isEmpty();
+        }
+    }
 
-	/**
-	 * @deprecated Will be removed in 1.1 due to performance issues
-	 *
-	 * @return the trace meta data and the trace events
-	 */
-	@Deprecated
-	public TraceEventRecords toTraceEvents() {
-		synchronized (this) {
-			// BETTER do not create a new array but use the events directly. Perhaps, we should refactor the events to an array.
-			final AbstractTraceEvent[] traceEvents = this.events.toArray(new AbstractTraceEvent[this.events.size()]);
-			return new TraceEventRecords(this.traceMetaData, traceEvents);
-		}
-	}
+    /**
+     * @deprecated Will be removed in 1.1 due to performance issues
+     *
+     * @return the trace meta data and the trace events
+     */
+    @Deprecated
+    public TraceEventRecords toTraceEvents() {
+        synchronized (this) {
+            // BETTER do not create a new array but use the events directly. Perhaps, we should
+            // refactor the events to an array.
+            final AbstractTraceEvent[] traceEvents = this.events.toArray(new AbstractTraceEvent[this.events.size()]);
+            return new TraceEventRecords(this.traceMetaData, traceEvents);
+        }
+    }
 
-	public List<AbstractTraceEvent> getTraceEvents() {
-		return events;
-	}
+    public List<AbstractTraceEvent> getTraceEvents() {
+        return this.events;
+    }
 
-	public TraceMetadata getTraceMetaData() {
-		return traceMetaData;
-	}
+    public TraceMetadata getTraceMetaData() {
+        return this.traceMetaData;
+    }
 
 }
