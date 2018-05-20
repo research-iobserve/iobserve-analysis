6f0091606860d9ecc2ca4096e2e75d773978e2ad
Lars Blümke <lbl@informatik.uni-kiel.de>
2018-05-20 15:33:39 +0200

    refactored model pre-processing filter

--- planning/src/main/java/org/iobserve/planning/stages/ModelProcessing.java
+++ planning/src/main/java/org/iobserve/planning/stages/ModelProcessing.java
@@ -27,9 +27,11 @@ import teetime.stage.basic.AbstractFilter;
 
 import org.apache.commons.io.FileUtils;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.iobserve.model.ModelHandlingErrorException;
 import org.iobserve.model.PCMModelHandler;
+import org.iobserve.model.factory.CostModelFactory;
 import org.iobserve.model.factory.DesignDecisionModelFactory;
 import org.iobserve.planning.data.AllocationGroup;
 import org.iobserve.planning.data.AllocationGroupsContainer;
@@ -41,6 +43,7 @@ import org.palladiosimulator.pcm.cloud.pcmcloud.cloudprofile.CloudProvider;
 import org.palladiosimulator.pcm.cloud.pcmcloud.cloudprofile.CloudResourceType;
 import org.palladiosimulator.pcm.cloud.pcmcloud.cloudprofile.VMType;
 import org.palladiosimulator.pcm.cloud.pcmcloud.resourceenvironmentcloud.ResourceContainerCloud;
+import org.palladiosimulator.pcm.core.composition.AssemblyContext;
 import org.palladiosimulator.pcm.resourceenvironment.LinkingResource;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceContainer;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
@@ -66,14 +69,22 @@ public class ModelProcessing extends AbstractFilter<File> {
     public static final int POSSIBLE_REPLICAS_OFFSET = 10;
     public static final int POSSIBLE_REPLICAS_FACTOR = 1;
 
+    private PCMModelHandler processedModelHandler;
+
     private AllocationGroupsContainer originalAllocationGroups;
     private Allocation allocationModel;
     private CloudProfile cloudProfileModel;
     private CostRepository costModel;
-    private ResourceEnvironment resourceEnvironmentModel;
     private DecisionSpace decisionModel;
+    private ResourceEnvironment resourceEnvironmentModel;
+    private org.palladiosimulator.pcm.system.System systemModel;
 
-    private File processedModelDir;
+    private String allocationModelName;
+    private String costModelName;
+    private String decisionSpaceName;
+    private String resourceEnvironmentName;
+
+    private File processedModelDirectory;
 
     /**
      * Performs the actual model transformation.
@@ -91,61 +102,89 @@ public class ModelProcessing extends AbstractFilter<File> {
      * all available resource containers as viable choices.
      *
      * @throws IOException
-     *             if an error occured while saving the models
+     *             if an error occurred while saving the models
      * @throws InitializationException
      *             if an error occurred during snapshot generation
+     * @throws ModelHandlingErrorException
+     *             if an err occurred during resource container creation
      */
     @Override
-    protected void execute(final File modelDirectory) throws IOException, InitializationException {
-        this.initModelTransformation(modelDirectory);
+    protected void execute(final File originalModelDirectory)
+            throws IOException, InitializationException, ModelHandlingErrorException {
+        this.saveModelNames(originalModelDirectory);
+        this.initModelTransformation(originalModelDirectory);
         this.clearUnneededElements();
         this.rebuildEnvironment();
 
-        this.outputPort.send(this.processedModelDir);
+        this.outputPort.send(this.processedModelDirectory);
+    }
+
+    private void saveModelNames(final File pcmModelsDirectory) {
+        final File[] files = pcmModelsDirectory.listFiles();
+        for (final File nextFile : files) {
+            final String filename = nextFile.getName();
+            final String extension = filename.substring(filename.lastIndexOf('.') + 1, filename.length());
+            if (PCMModelHandler.ALLOCATION_SUFFIX.equalsIgnoreCase(extension)) {
+                this.allocationModelName = filename;
+            } else if (PCMModelHandler.RESOURCE_ENVIRONMENT_SUFFIX.equalsIgnoreCase(extension)) {
+                this.resourceEnvironmentName = filename;
+            } else if (PCMModelHandler.COST_SUFFIX.equalsIgnoreCase(extension)) {
+                this.costModelName = filename;
+            } else if (PCMModelHandler.DESIGN_DECISION_SUFFIX.equalsIgnoreCase(extension)) {
+                this.decisionSpaceName = filename;
+            }
+
+        }
     }
 
     private void initModelTransformation(final File originalModelDirectory)
             throws IOException, InitializationException {
-        final PCMModelHandler originalModelHandler;
-        final PCMModelHandler processedModelHandler;
-        DecisionSpace decisionSpace;
+        final PCMModelHandler originalModelHandler = new PCMModelHandler(originalModelDirectory);
 
-        originalModelHandler = new PCMModelHandler(originalModelDirectory);
-        this.processedModelDir = new File(originalModelDirectory, ModelProcessing.PROCESSED_MODEL_FOLDER);
+        this.processedModelDirectory = new File(originalModelDirectory, ModelProcessing.PROCESSED_MODEL_FOLDER);
 
-        FileUtils.copyDirectory(originalModelDirectory, this.processedModelDir);
+        FileUtils.copyDirectory(originalModelDirectory, this.processedModelDirectory);
 
-        processedModelHandler = new PCMModelHandler(this.processedModelDir);
+        this.processedModelHandler = new PCMModelHandler(this.processedModelDirectory);
 
-        this.allocationModel = processedModelHandler.getAllocationModel();
-        this.cloudProfileModel = processedModelHandler.getCloudProfileModel();
-        this.costModel = processedModelHandler.getCostModel();
-        this.resourceEnvironmentModel = processedModelHandler.getResourceEnvironmentModel();
-        this.decisionModel = processedModelHandler.getDesignDecisionModel();
-        decisionSpace = processedModelHandler.getDesignDecisionModel();
+        this.allocationModel = this.processedModelHandler.getAllocationModel();
+        this.cloudProfileModel = this.processedModelHandler.getCloudProfileModel();
+        this.costModel = this.processedModelHandler.getCostModel();
+        this.decisionModel = this.processedModelHandler.getDesignDecisionModel();
+        this.resourceEnvironmentModel = this.processedModelHandler.getResourceEnvironmentModel();
+        this.systemModel = this.processedModelHandler.getSystemModel();
 
-        if (decisionSpace == null) {
-            decisionSpace = DesignDecisionModelFactory.createDecisionSpace("processedDecision");
+        if (this.decisionModel == null) {
+            this.decisionModel = DesignDecisionModelFactory.createDecisionSpace("processedDecision");
         }
 
-        final Allocation originalAllocation = originalModelHandler.getAllocationModel();
-
-        this.originalAllocationGroups = new AllocationGroupsContainer(originalAllocation);
+        this.originalAllocationGroups = new AllocationGroupsContainer(originalModelHandler.getAllocationModel());
     }
 
     private void clearUnneededElements() {
+        this.clearCosts();
         this.clearCloudResourcesFromResourceEnv();
+        this.clearAllocationContexts();
     }
 
-    private void rebuildEnvironment() {
+    private void rebuildEnvironment() throws ModelHandlingErrorException {
         for (final AllocationGroup allocationGroup : this.originalAllocationGroups.getAllocationGroups()) {
             final AllocationContext representingContext = allocationGroup.getRepresentingContext();
 
-            try {
-                this.createResourcesAndReplicationDegrees(this.decisionModel, allocationGroup);
-            } catch (final ModelHandlingErrorException e) {
-                ModelProcessing.LOGGER.error("Couldn't create ResourceContainer from VMType.");
+            // Set assembly context from allocation group to the context from the processed model
+            final AssemblyContext representedAsmCtx = this.systemModel
+                    .getAssemblyContexts__ComposedStructure().stream().filter(ctx -> representingContext
+                            .getAssemblyContext_AllocationContext().getId().equals(ctx.getId()))
+                    .findFirst().orElse(null);
+
+            if (representedAsmCtx == null) {
+                throw new RuntimeException(
+                        "There was something wrong with the system model. Please check your models.");
             }
+
+            representingContext.setAssemblyContext_AllocationContext(representedAsmCtx);
+
+            this.createResourcesAndReplicationDegrees(this.decisionModel, allocationGroup);
 
             this.allocationModel.getAllocationContexts_Allocation().add(representingContext);
         }
@@ -164,25 +203,26 @@ public class ModelProcessing extends AbstractFilter<File> {
     }
 
     private void saveModels() {
-        // new DesignDecisionModelHandler().save(
-        // this.processedModelDir.appendFileExtension(IPCMModelHandler.DESIGN_DECISION_SUFFIX),
-        // this.decisionModel);
-        // new AllocationModelHandler().save(
-        // this.processedModelDir.appendFileExtension(IPCMModelHandler.ALLOCATION_SUFFIX),
-        // this.allocationModel);
-        // new
-        // CostModelHandler().save(this.processedModelDir.appendFileExtension(IPCMModelHandler.COST_SUFFIX),
-        // this.costModel);
-        // new ResourceEnvironmentModelHandler().save(
-        // this.processedModelDir.appendFileExtension(IPCMModelHandler.RESOURCE_ENVIRONMENT_SUFFIX),
-        // this.resourceEnvironmentModel);
+        final URI decisionModelURI = URI
+                .createFileURI(new File(this.processedModelDirectory, this.decisionSpaceName).getAbsolutePath());
+        final URI allocationModelURI = URI
+                .createFileURI(new File(this.processedModelDirectory, this.allocationModelName).getAbsolutePath());
+        final URI costModelURI = URI
+                .createFileURI(new File(this.processedModelDirectory, this.costModelName).getAbsolutePath());
+        final URI resourceenvironmentModelURI = URI
+                .createFileURI(new File(this.processedModelDirectory, this.resourceEnvironmentName).getAbsolutePath());
+        this.processedModelHandler.save(decisionModelURI);
+        this.processedModelHandler.save(allocationModelURI);
+        this.processedModelHandler.save(costModelURI);
+        this.processedModelHandler.save(resourceenvironmentModelURI);
+
     }
 
     private void createResourcesAndReplicationDegrees(final DecisionSpace decisionSpace,
             final AllocationGroup allocationGroup) throws ModelHandlingErrorException {
-        final CloudProfile profile = this.cloudProfileModel;
-        final ResourceEnvironment environment = this.resourceEnvironmentModel;
-        final CostRepository costs = this.costModel;
+        // final CloudProfile profile = this.cloudProfileModel;
+        // final ResourceEnvironment environment = this.resourceEnvironmentModel;
+        // final CostRepository costs = this.costModel;
 
         // Get resource container that represents the instances this allocation
         // group is currently deployed on
@@ -198,7 +238,7 @@ public class ModelProcessing extends AbstractFilter<File> {
         // resource environment
         representingContainer.setGroupName(allocationGroup.getName());
         representingContainer.setEntityName(allocationGroup.getName());
-        ModelHelper.addResourceContainerToEnvironment(environment, representingContainer);
+        ModelHelper.addResourceContainerToEnvironment(this.resourceEnvironmentModel, representingContainer);
 
         final int nrOfCurrentReplicas = allocationGroup.getAllocationContexts().size();
 
@@ -210,22 +250,30 @@ public class ModelProcessing extends AbstractFilter<File> {
         // Create a new resource container with replication degree for each
         // VMType in the cloud profile, except for the one already added as the
         // representing container create only replication degree
-        for (final CloudProvider provider : profile.getCloudProviders()) {
+        for (final CloudProvider provider : this.cloudProfileModel.getCloudProviders()) {
             for (final CloudResourceType cloudResource : provider.getCloudResources()) {
                 if (cloudResource instanceof VMType) {
                     final VMType cloudVM = (VMType) cloudResource;
+                    final String degreeName;
+                    final ResourceContainerCloud createdContainer;
 
-                    String degreeName; // NOCS
-                    ResourceContainerCloud createdContainer; // NOCS
                     if (this.isSameVMType(cloudVM, representingContainer)) {
                         createdContainer = representingContainer;
+                        // If we just use the container, the references in the
+                        // resulting environment would point to the original cloudprofile
+                        createdContainer.setInstanceType(cloudVM);
+
+                        CostModelFactory.createFixedProcessingResourceCost(this.costModel, 0.0,
+                                cloudVM.getPricePerHour(),
+                                createdContainer.getActiveResourceSpecifications_ResourceContainer().get(0));
+
                         degreeName = String.format("%s_ReplicationDegree", allocationGroup.getName());
                     } else {
                         final String containerName = AllocationGroup.getAllocationGroupName(
                                 allocationGroup.getComponentName(),
                                 ModelHelper.getResourceContainerIdentifier(cloudVM));
-                        createdContainer = ModelHelper.createResourceContainerFromVMType(environment, costs, cloudVM,
-                                containerName);
+                        createdContainer = ModelHelper.createResourceContainerFromVMType(this.resourceEnvironmentModel,
+                                this.costModel, cloudVM, containerName);
                         degreeName = String.format("%s_ReplicationDegree", containerName);
                     }
 
@@ -241,10 +289,17 @@ public class ModelProcessing extends AbstractFilter<File> {
         return EcoreUtil.equals(cloudVM, representingContainer.getInstanceType());
     }
 
-    private void clearCloudResourcesFromResourceEnv() {
-        final ResourceEnvironment environment = this.resourceEnvironmentModel;
+    private void clearAllocationContexts() {
+        this.allocationModel.getAllocationContexts_Allocation().clear();
+    }
 
-        final List<ResourceContainer> resourceContainers = environment.getResourceContainer_ResourceEnvironment();
+    private void clearCosts() {
+        this.costModel.getCost().clear();
+    }
+
+    private void clearCloudResourcesFromResourceEnv() {
+        final List<ResourceContainer> resourceContainers = this.resourceEnvironmentModel
+                .getResourceContainer_ResourceEnvironment();
         for (final Iterator<ResourceContainer> iter = resourceContainers.iterator(); iter.hasNext();) {
             final ResourceContainer container = iter.next();
             if (container instanceof ResourceContainerCloud) {
@@ -252,7 +307,7 @@ public class ModelProcessing extends AbstractFilter<File> {
             }
         }
 
-        final LinkingResource linkingResource = ModelHelper.getInternetLinkingResource(environment);
+        final LinkingResource linkingResource = ModelHelper.getInternetLinkingResource(this.resourceEnvironmentModel);
         linkingResource.getConnectedResourceContainers_LinkingResource().clear();
     }
 }
