5acf368583f8eadc40e1fded89a952c8e34ee373
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-08-14 14:38:16 +0200

    prototyped correspondence model lookup for data type entries.

--- service.privacy.violation/src/main/java/org/iobserve/service/privacy/violation/filter/ProbeMapper.java
+++ service.privacy.violation/src/main/java/org/iobserve/service/privacy/violation/filter/ProbeMapper.java
@@ -24,8 +24,6 @@ import teetime.framework.AbstractConsumerStage;
 import teetime.framework.OutputPort;
 
 import org.iobserve.model.correspondence.CorrespondenceModel;
-import org.iobserve.model.correspondence.CorrespondencePackage;
-import org.iobserve.model.correspondence.OperationEntry;
 import org.iobserve.model.persistence.neo4j.DBException;
 import org.iobserve.model.persistence.neo4j.InvocationException;
 import org.iobserve.model.persistence.neo4j.ModelResource;
@@ -35,8 +33,11 @@ import org.iobserve.utility.tcp.events.TcpActivationControlEvent;
 import org.iobserve.utility.tcp.events.TcpActivationParameterControlEvent;
 import org.iobserve.utility.tcp.events.TcpDeactivationControlEvent;
 import org.palladiosimulator.pcm.allocation.AllocationContext;
+import org.palladiosimulator.pcm.repository.CollectionDataType;
+import org.palladiosimulator.pcm.repository.CompositeDataType;
 import org.palladiosimulator.pcm.repository.DataType;
 import org.palladiosimulator.pcm.repository.OperationSignature;
+import org.palladiosimulator.pcm.repository.PrimitiveDataType;
 import org.palladiosimulator.pcm.repository.Repository;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
 
@@ -127,21 +128,42 @@ public class ProbeMapper extends AbstractConsumerStage<ProbeManagementData> {
         final DataType returnType = operationSignature.getReturnType__OperationSignature();
         // TODO map model level returnType to code level returnType
         // TODO toString is not a good idea
-        this.correspondenceResource.findObjectsByTypeAndName(OperationEntry.class,
-                CorrespondencePackage.Literals.OPERATION_ENTRY, "name oder entityName", returnType.toString());
+        final List<DataTypeEntry> dataTypeEntries = this.correspondenceResource.collectAllObjectsByType(clazz, eClass);
+        final String codeLevelReturnType = this.getCodeLevelReturnType(dataTypeEntries, returnType);
 
         final String methodSignature = operationSignature.getEntityName();
         // TODO parameters
         final String parameterString = "*";
 
-        // TODO right identifier? and maybe resolve?
+        // TODO right identifier? and must resolve
         final String componentIdentifier = allocation.getAssemblyContext_AllocationContext()
                 .getEncapsulatedComponent__AssemblyContext().getRepository__RepositoryComponent().getEntityName();
 
         // TODO map component identifier
 
-        return modifier + " " + returnType + " " + componentIdentifier + "." + methodSignature + "(" + parameterString
-                + ")";
+        return modifier + " " + codeLevelReturnType + " " + componentIdentifier + "." + methodSignature + "("
+                + parameterString + ")";
+    }
+
+    private String getCodeLevelReturnType(final List<DataTypeEntry> dataTypes, final DataType returnType) {
+        for (final DataTypeEntry dataType : dataTypes) {
+            if (dataType.getOperation().getEntityName().equals(this.getTypeName(returnType))) {
+                return dataType.getImplementationId();
+            }
+        }
+    }
+
+    private String getTypeName(final DataType returnType) throws Exception {
+        if (returnType instanceof PrimitiveDataType) {
+            return ((PrimitiveDataType) returnType).getType().getLiteral(); // oder name
+        } else if (returnType instanceof CollectionDataType) {
+            return ((CollectionDataType) returnType).getEntityName();
+        } else if (returnType instanceof CompositeDataType) {
+            return ((CompositeDataType) returnType).getEntityName();
+        } else {
+            throw new Exception("XXXXX");
+        }
+
     }
 
     private TcpActivationControlEvent createActivationEvent(final String pattern, final List<String> whitelist) {
